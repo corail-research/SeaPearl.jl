bd5c72fd1cf9607e10ca10fb8cafa6ce219e5b38
Ilan Coulon <ilan.coulon@gmail.com>
2020-05-14 15:12:08 +0200

    Specify new_constraints to fixPoint

--- src/CP/core/fixPoint.jl
+++ src/CP/core/fixPoint.jl
@@ -1,16 +1,26 @@
 """
-    fixPoint!(model::CPModel)
+    fixPoint!(model::CPModel, new_constraints=nothing)
 
 Run the fix-point algorithm. Will prune the domain of every variable of `model` as much
 as possible, using its constraints.
+
+# Arguments
+- `model::CPModel`: the model you want to apply the algorithm on.
+- `new_constraints::Union{Array{Constraint}, Nothing}`: if this is filled with a set of constraints, 
+only those will be propagated in the first place.
 """
-function fixPoint!(model::CPModel)
+function fixPoint!(model::CPModel, new_constraints=nothing)
     toPropagate = Set{Constraint}()
 
-    for constraint in model.constraints
-        if constraint.active
-            push!(toPropagate, constraint)
+    # If we did not specify the second argument, it is the beginning so we propagate every constraint
+    if isnothing(new_constraints)
+        for constraint in model.constraints
+            if constraint.active
+                push!(toPropagate, constraint)
+            end
         end
+    else
+        union!(toPropagate, new_constraints)
     end
 
     while !isempty(toPropagate)
--- test/CP/core/fixPoint.jl
+++ test/CP/core/fixPoint.jl
@@ -6,23 +6,36 @@ using CPRL
         x = CPRL.IntVar(2, 6, trailer)
         y = CPRL.IntVar(5, 8, trailer)
         z = CPRL.IntVar(6, 15, trailer)
+        t = CPRL.IntVar(6, 10, trailer)
 
         constraint = CPRL.Equal(x, y)
-        constraint2 = CPRL.Equal(y, z)
+        
+        constraint3 = CPRL.Equal(z, t)
 
         model = CPRL.CPModel()
 
         push!(model.variables, x)
         push!(model.variables, y)
         push!(model.variables, z)
+        push!(model.variables, t)
 
         push!(model.constraints, constraint)
+        push!(model.constraints, constraint3)
+        CPRL.fixPoint!(model)
+
+        @test length(x.domain) == 2
+        @test length(y.domain) == 2
+        @test length(z.domain) == 5
+        @test length(t.domain) == 5
+
+        constraint2 = CPRL.Equal(y, z)
         push!(model.constraints, constraint2)
 
-        CPRL.fixPoint!(model)
+        CPRL.fixPoint!(model, [constraint2])
 
         @test CPRL.isbound(x)
         @test CPRL.isbound(y)
         @test CPRL.isbound(z)
+        @test CPRL.isbound(t)
     end
 end
