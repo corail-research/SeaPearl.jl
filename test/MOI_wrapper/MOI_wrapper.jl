ccc98556b1fd0c897e87803a111592d214a79bc9
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-05-16 14:01:38 +0200

    test add variable

--- test/MOI_wrapper/MOI_wrapper.jl
+++ test/MOI_wrapper/MOI_wrapper.jl
@@ -1,4 +1,12 @@
-@testset "MOI.jl" begin
+@testset "MOI_wrapper.jl" begin
+
+    @testset "Adding variables" begin
+        model = CPRL.Optimizer()
+        CPRL.MOI.add_variable(model)
+        @test "1" in keys(model.cpmodel.variables)
+        @test !("2" in keys(model.cpmodel.variables))
+    end
+
     @testset "add_constraint()" begin
         @test CPRL.add_constraint() == nothing
         @test_logs (:info, "Constraint added !") CPRL.add_constraint()
