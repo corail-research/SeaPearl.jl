06c9ed68f5ce57dd7c9170b3fc5e62840384c392
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-05-18 15:40:35 +0200

    interface for constraint: two variables equal or different

--- src/MOI_wrapper/MOI_wrapper.jl
+++ src/MOI_wrapper/MOI_wrapper.jl
@@ -95,5 +95,38 @@ function MOI.add_constraint(model::Optimizer, sgvar::MOI.SingleVariable, set::No
     return MOI.ConstraintIndex{MOI.SingleVariable, MOI.NotEqualTo}(constraint_index)
 end
 
+# if true, variables are equal, else they are different
+struct VariablesEquality <: MOI.AbstractScalarSet
+    value::Bool
+end
+
+"""
+    MOI.add_constraint(model::Optimizer, sgvar::MOI.VectorOfVariables, set::VariablesEquality)
+
+Interface function which add a constraint to the model (which is himself an Optimizer)
+This constraints a single variable to be different or equal to another variable.
+"""
+function MOI.add_constraint(model::Optimizer, vectOfVar::MOI.VectorOfVariables, set::VariablesEquality)
+    # get the VariableIndex and convert it to string
+    @assert MOI.output_dimension(vectOfVar) == 2
+
+    id1, id2 = string(vectOfVar.variables[1]), string(vectOfVar.variables[2])
+    equal = set.value
+
+    # create the constraint
+    if equal
+        constraint = Equal(model.cpmodel.variables[id1], model.cpmodel.variables[id1], model.cpmodel.trailer)
+    else
+        constraint = NotEqual(model.cpmodel.variables[id1], model.cpmodel.variables[id1], model.cpmodel.trailer)
+
+    # add constraint to the model
+    push!(model.cpmodel.constraints, constraint)
+
+    # return the constraint Index (asked by MathOptInterface)
+    constraint_index = length(model.cpmodel.constraints) + 1
+    return MOI.ConstraintIndex{MOI.SingleVariable, MOI.NotEqualTo}(constraint_index)
+end
+
+
 add_constraint() = @info "Constraint added !"
 
