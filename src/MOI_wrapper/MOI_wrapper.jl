912d4fe4fdb5c0cca3efe9ae54a7ea9badf78302
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-05-18 13:20:33 +0200

    add constraint different than and variables are now added constrained

--- src/MOI_wrapper/MOI_wrapper.jl
+++ src/MOI_wrapper/MOI_wrapper.jl
@@ -8,7 +8,7 @@ WIP
 - is MOI.VariableIndex necessary ?: we should maybe use VariableIndex in our code to allow
 easier use of it in the MathOptInterface (https://www.juliaopt.org/MathOptInterface.jl/dev/apireference/#Index-types-1)
 Should create a new type of Variables here or change IntVar's implementation. Not sure atm.
-- 
+- not sure if it is compulsory to return a constraint while coding add_constrained_variable
 
 """
 
@@ -26,25 +26,28 @@ end
 
 Interface function which add a variable to the model (which is himself an Optimizer)
 """
-function MOI.add_variable(model::Optimizer)
+function MOI.add_constrained_variable(model::Optimizer, set::MOI.Interval)
     """
-    Tried with min = 0 and max = typemax(Int) but got an Array size error.
-    Should think about the min/max initialisation. I temporarly put 2048 as max.
+    Might be compulsory to create a new constraint: variable in interval, to create it 
+    in this function and to throw it at the end (instead of nothing)
     """
-    MAX_SIZE = 2048
+    # create new id
     id = string(length(keys(model.cpmodel.variables)) + 1)
-    newvariable = CPRL.IntVar(0, 1, id, model.cpmodel.trailer)
-    CPRL.addVariable!(model.cpmodel, newvariable)
-    return MOI.VariableIndex(parse(Int, id))
-end
 
-MOI.add_variables(model::Optimizer, n::Int) = [MOI.add_variable(model) for i = 1:n]
+    # get information from the set
+    min = set.lower
+    max = set.upper
+
+    newvariable = CPRL.IntVar(min, max, id, model.cpmodel.trailer)
+    CPRL.addVariable!(model.cpmodel, newvariable)
+    return MOI.VariableIndex(parse(Int, id)), nothing
+end
 
 """
     MOI.add_constraint(model::Optimizer, sgvar::MOI.SingleVariable, set::MOI.EqualTo)
 
 Interface function which add a constraint to the model (which is himself an Optimizer).
-This constraints a single variable to be different than a given Integer.  
+This constraints a single variable to be equal to a given Integer.  
 """
 function MOI.add_constraint(model::Optimizer, sgvar::MOI.SingleVariable, set::MOI.EqualTo)
     # get the VariableIndex and convert it to string
@@ -62,27 +65,30 @@ function MOI.add_constraint(model::Optimizer, sgvar::MOI.SingleVariable, set::MO
     return MOI.ConstraintIndex{MOI.SingleVariable, MOI.EqualTo}(constraint_index)
 end
 
+struct NotEqualTo <: MOI.AbstractScalarSet
+    value::Int
+end
+
 """
-    MOI.add_constraint(model::Optimizer, sgvar::MOI.SingleVariable, set::MOI.Interval)
+    MOI.add_constraint(model::Optimizer, sgvar::MOI.SingleVariable, set::NotEqualTo)
 
 Interface function which add a constraint to the model (which is himself an Optimizer)
-This constraints a single variable to be in a given interval.
+This constraints a single variable to be equal to a given interval.
 """
-function MOI.add_constraint(model::Optimizer, sgvar::MOI.SingleVariable, set::MOI.Interval)
+function MOI.add_constraint(model::Optimizer, sgvar::MOI.SingleVariable, set::NotEqualTo)
     # get the VariableIndex and convert it to string
     id = string(sgvar.variable)
-    min = set.lower
-    max = set.upper
+    constant = set.value
 
     # create the constraint: need to create the native one first (WIP)
-    constraint = nothing
+    constraint = NotEqualConstant(model.cpmodel.variables[id], constant, model.cpmodel.trailer)
 
     # add constraint to the model
     push!(model.cpmodel.constraints, constraint)
 
     # return the constraint Index (asked by MathOptInterface)
     constraint_index = length(model.cpmodel.constraints) + 1
-    return MOI.ConstraintIndex{MOI.SingleVariable, MOI.EqualTo}(constraint_index)
+    return MOI.ConstraintIndex{MOI.SingleVariable, MOI.NotEqualTo}(constraint_index)
 end
 
 add_constraint() = @info "Constraint added !"
