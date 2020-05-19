7ce748ac825c87cc1b84db68bdb59412220dfdfa
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-05-19 17:10:53 +0200

    add tests for MOI_wrapper
    tests for optimizer creation, options creation, as well as variables creation and constraints creation

--- test/MOI_wrapper/MOI_wrapper.jl
+++ test/MOI_wrapper/MOI_wrapper.jl
@@ -1,13 +1,66 @@
+using MathOptInterface
+
+const MOI = MathOptInterface
+const MOIU = MathOptInterface.Utilities
+
 @testset "MOI_wrapper.jl" begin
+
+    @testset "Creating an optimizer" begin
+        model = CPRL.Optimizer()
+        @test MOI.get(model, MOI.SolverName()) == "CPRL Solver"
+    end
+
+    @testset "Giving parameters to the optimizer" begin
+        model = CPRL.Optimizer()
+        MOI.set(model, MOI.RawParameter("Test"), "test")
+        @test model.options["Test"] == "test"
+        MOI.empty!(model)
+        @test MOI.is_empty(model)
+        @test model.options["Test"] == "test"
+    end
 
     @testset "Adding constrained variables" begin
         model = CPRL.Optimizer()
-        CPRL.MOI.add_constrained_variable(model, CPRL.MOI.Interval(1, 12))
-        @test "1" in keys(model.cpmodel.variables)
-        @test !("2" in keys(model.cpmodel.variables))
+        MOI.add_constrained_variable(model, MOI.Interval(1, 4))
+        MOI.add_constrained_variable(model, MOI.Interval(1, 4))
+        MOI.add_constrained_variable(model, MOI.Interval(1, 4))
+        MOI.add_constrained_variable(model, MOI.Interval(1, 4))
+        @test Set(keys(model.cpmodel.variables)) == Set(["1", "2", "3", "4"])
     end
 
-    @testset "add_constraint()" begin
+    @testset "Adding constraints" begin
+        model = CPRL.Optimizer()
+        MOI.add_constrained_variable(model, MOI.Interval(1, 4))
+        MOI.add_constrained_variable(model, MOI.Interval(1, 4))
+        MOI.add_constrained_variable(model, MOI.Interval(1, 4))
+        MOI.add_constrained_variable(model, MOI.Interval(1, 4))
+
+        # add new constraints
+        MOI.add_constraint(model, MOI.SingleVariable(MOI.VariableIndex(1)), MOI.LessThan(2))
+        MOI.add_constraint(model, MOI.SingleVariable(MOI.VariableIndex(2)), MOI.GreaterThan(2))
+        #MOI.add_constraint(model, MOI.SingleVariable(MOI.VariableIndex(4)), MOI.Interval(2, 3))
+
+        # use fixPoint 
+        CPRL.fixPoint!(model.cpmodel, model.cpmodel.constraints)
+
+        # test if it had effect on the variables' domains
+        @test model.cpmodel.variables[string(MOI.VariableIndex(1).value)].domain.max.value == 2
+        @test model.cpmodel.variables[string(MOI.VariableIndex(2).value)].domain.min.value == 2
+        #@test model.cpmodel.variables[string(MOI.VariableIndex(3).value)].domain.values == [2, 3]
+
+        # add some new constraints again
+        MOI.add_constraint(model, MOI.SingleVariable(MOI.VariableIndex(3)), MOI.EqualTo(2))
+        MOI.add_constraint(model, MOI.VectorOfVariables([MOI.VariableIndex(1), MOI.VariableIndex(3)]), CPRL.VariablesEquality(false))
+
+        # perform another fixPoint
+        CPRL.fixPoint!(model.cpmodel, model.cpmodel.constraints)
+
+        # new bunch of test
+        @test CPRL.isbound(model.cpmodel.variables[string(MOI.VariableIndex(1).value)])
+        @test model.cpmodel.variables[string(MOI.VariableIndex(3).value)].domain.values == [2]
+    end
+
+    @testset "Optimizing" begin
         @test CPRL.add_constraint() == nothing
         @test_logs (:info, "Constraint added !") CPRL.add_constraint()
     end
