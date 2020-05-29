b0f4106fc81b1dbe383e7df632bf1ddab20d6931
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-05-29 19:01:39 +0200

    create basic dqn agent constructor

--- examples/RLplayground/dqn.jl
+++ examples/RLplayground/dqn.jl
@@ -16,43 +16,26 @@ ns, na = length(rand(get_observation_space(env))), length(get_action_space(env))
 
 hidden_size = 32
 
+# create an agent 
+smart_agent = CPRL.BasicDQNAgent(ns, ns, hidden_size)
 
-agent = Agent(
-    policy = QBasedPolicy(
-        learner = BasicDQNLearner(
-            approximator = NeuralNetworkApproximator(
-                model = Chain(
-                    Dense(ns, hidden_size, relu; initW = seed_glorot_uniform(seed = 17)),
-                    Dense(hidden_size, hidden_size, relu; initW = seed_glorot_uniform(seed = 23)), 
-                    Dense(hidden_size, na; initW = seed_glorot_uniform(seed = 39))
-                ),
-                optimizer = ADAM()
-            ),
-            batch_size = 32,
-            min_replay_history = 100,
-            loss_func = huber_loss,
-            seed = 22,
-        ), 
-        explorer = EpsilonGreedyExplorer(
-            kind = :exp,
-            ϵ_stable = 0.01,
-            decay_steps = 500,
-            seed = 33
-        )
-    ),
-    trajectory = CircularCompactSARTSATrajectory(
-        capacity = 1000, 
-        state_type = Float32,
-        #state_type = Float64, 
-        state_size = (ns,)
-    )
+# compare with a random agent
+random_agent = Agent(
+    policy = RandomPolicy(env; seed=456),
+    trajectory = CircularCompactSARTSATrajectory(; capacity=3, state_type=Float32, state_size = (4,)),
 )
+hook = ComposedHook(TotalRewardPerEpisode(), TimePerStep())
+run(random_agent, env, StopAfterEpisode(200), hook)
+@info "Stats for RandomAgent" avg_reward = mean(hook[1].rewards) avg_last_rewards = mean(hook[1].rewards[end-10:end]) avg_fps = 1 / mean(hook[2].times)
 
+
+# create an hook
 hook = ComposedHook(TotalRewardPerEpisode(), TimePerStep())
 
-run(agent, env, StopAfterEpisode(200), hook)
+# run an experiment with a stop condition
+run(smart_agent, env, StopAfterEpisode(200), hook)
+@info "Stats for BasicDQNAgent" avg_reward = mean(hook[1].rewards) avg_last_rewards = mean(hook[1].rewards[end-10:end]) avg_fps = 1 / mean(hook[2].times)
 
-@info "Stats for BasicDQNLearner" avg_reward = mean(hook[1].rewards) avg_fps = 1 / mean(hook[2].times)
 
 p = plot(1:length(hook[1].rewards), hook[1].rewards, xlabel="Episode", ylabel="Reward")
 display(p)
--- src/RL/agent/basicdqnagent.jl
+++ src/RL/agent/basicdqnagent.jl
@@ -1,32 +1,50 @@
-function BasicDQNAgent()
+"""
+    BasicDQNAgent(state_space_size, action_space_size, hidden_size = 32; 
+                optimizer = ADAM(), 
+                batch_size = 32, min_replay_history = 100, loss_func = huber_loss, learner_seed = 22,
+                kind = :exp, ϵ_stable = 0.01, decay_steps = 500, explorer_seed = 33,
+                capacity = 1000, state_type = Float32, state_size = (state_space_size,)
+                )
+
+Using the structure of ReinforcementLearning.jl agents. This is the structure of a basic 
+DQN agent (the one from Playing Atari - Google Deep Mind). This function give the possibility 
+to parametrize this agent without having to think about the entire structure. 
+If user, wants to go further, he can create its own agent. 
+"""
+function BasicDQNAgent(state_space_size, action_space_size, hidden_size = 32; 
+                        optimizer = ADAM, 
+                        batch_size = 32, min_replay_history = 100, loss_func = huber_loss, learner_seed = 22,
+                        kind = :exp, ϵ_stable = 0.01, decay_steps = 500, explorer_seed = 33,
+                        capacity = 1000, state_type = Float32, state_size = (state_space_size,)
+                        )
+    # function
     agent = RL.Agent(
         policy = RL.QBasedPolicy(
             learner = RL.BasicDQNLearner(
                 approximator = RL.NeuralNetworkApproximator(
                     model = Chain(
-                        Dense(ns, hidden_size, relu; initW = seed_glorot_uniform(seed = 17)),
+                        Dense(state_space_size, hidden_size, relu; initW = seed_glorot_uniform(seed = 17)),
                         Dense(hidden_size, hidden_size, relu; initW = seed_glorot_uniform(seed = 23)), 
-                        Dense(hidden_size, na; initW = seed_glorot_uniform(seed = 39))
+                        Dense(hidden_size, action_space_size; initW = seed_glorot_uniform(seed = 39))
                     ),
-                    optimizer = ADAM()
+                    optimizer = optimizer()
                 ),
-                batch_size = 32,
-                min_replay_history = 100,
-                loss_func = huber_loss,
-                seed = 22,
+                batch_size = batch_size,
+                min_replay_history = min_replay_history,
+                loss_func = loss_func,
+                seed = learner_seed,
             ), 
             explorer = RL.EpsilonGreedyExplorer(
-                kind = :exp,
-                ϵ_stable = 0.01,
-                decay_steps = 500,
-                seed = 33
+                kind = kind,
+                ϵ_stable = ϵ_stable,
+                decay_steps = decay_steps,
+                seed = explorer_seed
             )
         ),
         trajectory = RL.CircularCompactSARTSATrajectory(
-            capacity = 1000, 
-            #state_type = Float32,
-            state_type = Float64, 
-            state_size = (ns,)
+            capacity = capacity, 
+            state_type = state_type, 
+            state_size = state_size
         )
     )
     return agent
