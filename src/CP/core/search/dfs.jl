5988a21cf3e5503f776ecef51757a77b4ca3eec9
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-06-12 18:56:42 +0200

    WIP: add the RL functions inside the search procedure

--- src/CP/core/search/dfs.jl
+++ src/CP/core/search/dfs.jl
@@ -1,22 +1,30 @@
 abstract type DFSearch <: SearchStrategy end
 
 """
-    search!(model::CPModel, ::Type{DFSearch}, variableHeuristic)
+    search!(model::CPModel, ::Type{DFSearch}, variableHeuristic, agent::RL.Agent)
 
 Perform a Depth-First search in the `model` using `variableHeuristic` to choose which domain will be changed
 at each branching. This strategy, starting at the root node, will explore as deep as possible before backtracking.
 """
-function search!(model::CPModel, ::Type{DFSearch}, variableHeuristic)
+function search!(model::CPModel, ::Type{DFSearch}, variableHeuristic, agent::RL.Agent)
 
     # the agent will probably be given as a parameter of the search function
 
-    # create the environment (will be under valueSelection later)
-    # env = RLEnv(model::CPModel)
+    # create the environment: state is taken randomly but will be synchronized later
+    env = RLEnv(model::CPModel)
+    false_x = first(values(model.variables))
+    sync_state!(env, model, false_x)
+    obs = observe(env, false_x)
+    agent(RL.PRE_EPISODE_STAGE, obs) # just empty the buffer
+    # eventually hook(PRE_EPISODE_STAGE, agent, env, obs)
+
+    # the first decision will be taken after the first call at the fixPoint!
+    # nothing here 
 
     toCall = Stack{Function}()
 
     # Starting at the root node with an empty stack
-    currentStatus = expandDfs!(toCall, model, variableHeuristic)
+    currentStatus = expandDfs!(toCall, model, variableHeuristic, env, agent)
     
     while !isempty(toCall)
         # Stop right away if reached a limit
@@ -24,9 +32,29 @@ function search!(model::CPModel, ::Type{DFSearch}, variableHeuristic)
             break
         end
 
+        # the RL EPISODE continue
+        # change reward in case of :Unfeasible status (I would like it for :FoundSolution if possible)
+        # if is unnecessary but i keep it for visual issue atm 
+        if currentStatus in [:Unfeasible, :FoundSolution]
+            set_reward!(env, currentStatus)
+            # when we go back to expandDfs, env will be able to add the reward to the observation
+        end
+
         currentProcedure = pop!(toCall)
         currentStatus = currentProcedure(model)
     end
+
+    # the RL EPISODE stops
+    set_done!(env, true)
+    set_final_reward!(env, model)
+    sync_state!(env, model, false_x)
+    obs = observe(env, false_x)
+    
+    agent(RL.POST_ACT_STAGE, obs) # get terminal and reward
+    # eventually: hook(POST_ACT_STAGE, agent, env, obs, action)
+    
+    agent(RL.POST_EPISODE_STAGE, obs)  # let the agent see the last observation
+    # eventually hook(POST_EPISODE_STAGE, agent, env, obs)
 
     if currentStatus == :NodeLimitStop || currentStatus == :SolutionLimitStop
         return currentStatus
@@ -41,12 +69,12 @@ function search!(model::CPModel, ::Type{DFSearch}, variableHeuristic)
 end
 
 """
-    expandDfs!(toCall::Stack{Function}, model::CPModel, variableHeuristic::Function, newConstraints=nothing)
+    expandDfs!(toCall::Stack{Function}, model::CPModel, variableHeuristic::Function, env::RLEnv, agent::RL.Agent, newConstraints=nothing)
 
 Add procedures to `toCall`, that, called in the stack order (LIFO) with the `model` parameter, will perform a DFS in the graph.
 Some procedures will contain a call to `expandDfs!` itself.
 """
-function expandDfs!(toCall::Stack{Function}, model::CPModel, variableHeuristic::Function, newConstraints=nothing)
+function expandDfs!(toCall::Stack{Function}, model::CPModel, variableHeuristic::Function, env::RLEnv, agent::RL.Agent, newConstraints=nothing)
     # Dealing with limits
     model.statistics.numberOfNodes += 1
     if !belowNodeLimit(model)
@@ -63,22 +91,29 @@ function expandDfs!(toCall::Stack{Function}, model::CPModel, variableHeuristic::
     end
     if solutionFound(model)
         triggerFoundSolution!(model)
-        return :Feasible
+        return :FoundSolution
     end
+
+    # All the transformations due to the next action have been done 
 
     # Variable selection
     x = variableHeuristic(model)
 
-    # in our framework, the changesare made by the cpmodel, thus, the env do not launch the 
+    # in our framework, the changes are made by the cpmodel, thus, the env do not launch the 
     # changes, that is why there is no env(action) here but there is a synchronisation of the env with
     # the cpmodel: sync!(env, model, x)
-    # sync!(env, model, x)
+    sync_state!(env, model, x)
 
-    # obs = observe(env, x)
-    # agent(POST_ACT_STAGE, obs) # get terminal and reward
-    # eventually: hook(POST_ACT_STAGE, agent, env, obs, action)
+    obs = observe(env, x)
+    if model.statistics.numberOfNodes >= 1
+        agent(RL.POST_ACT_STAGE, obs) # get terminal and reward
+        # eventually: hook(POST_ACT_STAGE, agent, env, obs, action)
+    end
 
-    # v = agent(PRE_ACT_STAGE, obs) # choose action, store it with the state
+    # here we should check if we are still in the game ! But by definition, we are in it.
+    # the hard part are the :Infeasible, as they are not here even if still in the game !
+
+    v = agent(RL.PRE_ACT_STAGE, obs) # choose action, store it with the state
     # eventually hook(PRE_ACT_STAGE, agent, env, obs, action)
 
     
@@ -87,11 +122,11 @@ function expandDfs!(toCall::Stack{Function}, model::CPModel, variableHeuristic::
 
 
     push!(toCall, (model) -> (restoreState!(model.trailer); :Feasible))
-    push!(toCall, (model) -> (remove!(x.domain, v); expandDfs!(toCall, model, variableHeuristic, getOnDomainChange(x))))
+    push!(toCall, (model) -> (remove!(x.domain, v); expandDfs!(toCall, model, variableHeuristic, env, agent, getOnDomainChange(x))))
     push!(toCall, (model) -> (saveState!(model.trailer); :Feasible))
 
     push!(toCall, (model) -> (restoreState!(model.trailer); :Feasible))
-    push!(toCall, (model) -> (assign!(x, v); expandDfs!(toCall, model, variableHeuristic, getOnDomainChange(x))))
+    push!(toCall, (model) -> (assign!(x, v); expandDfs!(toCall, model, variableHeuristic, env, agent, getOnDomainChange(x))))
     push!(toCall, (model) -> (saveState!(model.trailer); :Feasible))
 
     return :Feasible
