c7b1170598d8b84fd4c40b070295656f12686853
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-06-12 18:56:11 +0200

    WIP: adapt env to track informations during the search

--- src/RL/env/env.jl
+++ src/RL/env/env.jl
@@ -4,11 +4,18 @@
     RLEnv
 
 Implementation of the RL.AbstractEnv type coming from ReinforcementLearning's interface.
+The RLEnv does not need t (step) and action as they are usually useful to control what is 
+happening and this role is taken by the CP part in our framework. 
+
+We will keep the env.done for now for convenience reasons (to stay near enough to the RL 
+framework in order to be able to use its useful functions)
 """
 mutable struct RLEnv{T, R<:AbstractRNG} <: RL.AbstractEnv 
     action_space::RL.DiscreteSpace{UnitRange{Int64}}
     observation_space::CPGraphSpace
     state::CPGraph
+    action::Int64
+    reward::Int64
     done::Bool
     rng::R # random number generator
 end
@@ -32,8 +39,10 @@ function RLEnv(cpmodel::CPModel, seed = nothing)
     env = RLEnv(
         action_space,
         observation_space,
-        Random.rand(rng, observation_space), # will be synchronised later 
-        false,
+        Random.rand(rng, observation_space), # will be synchronised later
+        1,
+        -1,
+        false,  
         rng)
     
     RL.reset!(env)
@@ -50,16 +59,51 @@ function sync_state!(env::RLEnv, cpmodel::CPModel, x::AbstractIntVar)
     env.state = CPGraph(g, x)
 end
 
-
 """
-    RL.reset!(::RLEnv)
+    set_done!(env::RLEnv, done::Bool)
 
-Reinitialise the environment so it is ready for a new episode.
+Change the "done" attribute of the env. This is compulsory as used in the buffer
+for the training.
 """
-function reset!(env::RLEnv{T}) where {T<:Number}
-    # the state will be synchronised later
-    env.done = false
+function set_done!(env::RLEnv, done::Bool)
+    env.done = done
     nothing
+end
+
+"""
+    set_reward!(env::RLEnv, symbol::Symbol)
+
+Change the "reward" attribute of the env. This is compulsory as used in the buffer
+for the training.
+"""
+function set_reward!(env::RLEnv, symbol::Symbol)
+    env.reward += - 5 * (symbol == :Infeasible) + 5 * (symbol == :FoundSolution)
+    nothing
+end
+
+"""
+    set_final_reward!(env::RLEnv, symbol::Symbol)
+
+Change the "reward" attribute of the env. This is compulsory as used in the buffer
+for the training.
+"""
+function set_final_reward!(env::RLEnv, model::CPModel)
+    if isempty(model.solutions)
+        env.reward = 0
+    else
+        env.reward = model.objectiveBound + 1
+    end
+    nothing
+end
+
+"""
+    reset!(env::RLEnv)
+
+Not sure this one will survive
+"""
+function reset!(env::RLEnv)
+    env.done = false
+    nothing 
 end
 
 """
@@ -72,15 +116,16 @@ legal_actions !
 To do : Need to change the reward
 To do : Need to change the legal actions
 """
-function observe(env::RLEnv, x::AbstractIntVar)
+function observe!(env::RLEnv, x::AbstractIntVar)
     # get legal_actions_mask
-    legal_actions_mask = [value in x.domain ? true : false  for value in env.action_space]
+    legal_actions_mask = [value in x.domain for value in env.action_space]
 
     # compute legal actions
     legal_actions = env.action_space[legal_actions_mask]
 
     # compute reward - we could add a transition function given by the user
-    reward = env.done ? -1 : 0
+    reward = env.reward 
+    env.reward = -1
 
     # return the observation as a named tuple (useful for interface understanding)
     return (reward = reward, terminal = env.done, state = env.state, legal_actions = legal_actions, legal_actions_mask = legal_actions_mask)
