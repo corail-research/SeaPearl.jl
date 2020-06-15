55b22314dbcdb4a16a1073dc99a3ddf552e0289d
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-06-15 17:08:48 +0200

    CPGraph: create a new constructor and an update!() function

--- src/RL/env/spaces/cpgraph_space.jl
+++ src/RL/env/spaces/cpgraph_space.jl
@@ -14,12 +14,49 @@ Construct a CPGraph from the CPLayerGraph and the variable we want to branch on.
 Here we will define how the node feature will look like. Thus, it might be transformed later.
 """
 function CPGraph(g::CPLayerGraph, x::AbstractIntVar)
-    graph = LightGraphs.LinAlg.adjacency_matrix(g)
+    sparse_adj = LightGraphs.LinAlg.adjacency_matrix(g)
     # temporary use of a one hot encoder for each node. 
     feature = Matrix{Float32}(I, nv(g), nv(g))
     variable_id = index(g, VariableVertex(x))
     
-    CPGraph(GeometricFlux.FeaturedGraph(graph, feature), variable_id)
+    CPGraph(GeometricFlux.FeaturedGraph(sparse_adj, feature), variable_id)
+end
+
+"""
+    CPGraph(cplayergraph::CPLayerGraph, var_id::Int64)
+
+Construct a CPGraph from the CPLayerGraph and the variable we want to branch on. 
+Here we will define how the node feature will look like. Thus, it might be transformed later.
+"""
+function CPGraph(g::CPLayerGraph, var_id::Int64)
+    sparse_adj = LightGraphs.LinAlg.adjacency_matrix(g)
+    # temporary use of a one hot encoder for each node. 
+    feature = Matrix{Float32}(I, nv(g), nv(g))
+    
+    CPGraph(GeometricFlux.FeaturedGraph(sparse_adj, feature), var_id)
+end
+
+"""
+    update!(cpgraph::CPGraph, g::CPLayerGraph, x::AbstractIntVar)
+
+Update a CPGraph instance thanks to a new CPLayerGraph and a new Variable. 
+The only thing that stay identic is basically the feature matrix.
+
+---------- WARNING : 
+At the moment, we are using GeometricFlux.FeaturedGraph, which is immutable, so
+we could change the pointer ref or change elements after elements in the sparse 
+adjacency matrix. But in order to get the things done quickly, we will start by ignoring 
+the update!() function and we will create a new FeaturedGraph when we want to update
+CPGraph.
+"""
+function update!(cpgraph::CPGraph, g::CPLayerGraph, x::AbstractIntVar)
+    cpgraph.variable_id = index(g, VariableVertex(x))
+
+    feature = cpgraph.featuredgraph.feature[]
+    sparse_adj = LightGraphs.LinAlg.adjacency_matrix(g)
+
+    cpgraph.featuredgraph = GeometricFlux.FeaturedGraph(sparse_adj, feature)
+    nothing
 end
 
 """
@@ -42,7 +79,7 @@ end
 
 Create a `CPGraphSpace` with span of `1:high`.
 """
-CPGraphSpace(variable_id_high::Int64) = CPGraphSpace(1, variable_id_high)
+CPGraphSpace(variable_id_high::Int64) = CPGraphSpace(0, variable_id_high)
 
 
 Base.eltype(s::CPGraphSpace) = CPGraph
