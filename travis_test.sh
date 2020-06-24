af6feff831be44d045dbfd3774055c2e98cbdc30
Ilan Coulon <ilan.coulon@gmail.com>
2020-06-24 18:23:33 +0200

    Two-lines script

--- travis_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ev
-
-julia --color=yes -e "if VERSION < v\"0.7.0-DEV.5183\"; Pkg.clone(pwd()); Pkg.build(\"${JL_PKG}\"); else using Pkg; if VERSION >= v\"1.1.0-rc1\"; Pkg.build(verbose=true); else Pkg.build(); end; end"
-travis_wait julia --check-bounds=yes --color=yes -e "if VERSION < v\"0.7.0-DEV.5183\"; Pkg.test(\"${JL_PKG}\", coverage=true); else using Pkg; Pkg.test(coverage=true); end"
