e464b25ecf15f71a16b6271ea3b081252cf0ae4e
Ilan Coulon <ilan.coulon@gmail.com>
2020-07-08 12:55:49 +0200

    updating tests

--- test/RL/env/reward.jl
+++ test/RL/env/reward.jl
@@ -1,17 +1,7 @@
 struct TestReward <: CPRL.AbstractReward end
 
-function CPRL.set_backtracking_reward!(env::CPRL.RLEnv{TestReward}, model::CPRL.CPModel, current_status::Union{Nothing, Symbol})
-    env.reward += 5
-    nothing
-end
-
 function CPRL.set_before_next_decision_reward!(env::CPRL.RLEnv{TestReward}, model::CPRL.CPModel)
     env.reward += 3
-    nothing
-end
-
-function CPRL.set_after_decision_reward!(env::CPRL.RLEnv{TestReward}, model::CPRL.CPModel)
-    env.reward = 18
     nothing
 end
 
@@ -31,26 +21,6 @@ end
         env.reward = 0
         CPRL.set_before_next_decision_reward!(env, model)
         @test env.reward == -1/40
-    end
-    @testset "set_backtracking_reward!()" begin
-        trailer = CPRL.Trailer()
-        model = CPRL.CPModel(trailer)
-
-        env = CPRL.RLEnv(model)
-
-        env.reward = 0
-        CPRL.set_backtracking_reward!(env, model, :status)
-        @test env.reward == -1/80
-    end
-    @testset "set_after_decision_reward!()" begin
-        trailer = CPRL.Trailer()
-        model = CPRL.CPModel(trailer)
-
-        env = CPRL.RLEnv(model)
-
-        env.reward = 5
-        CPRL.set_after_decision_reward!(env, model)
-        @test env.reward == 0
     end
     @testset "set_final_reward!()" begin
         trailer = CPRL.Trailer()
@@ -74,26 +44,6 @@ end
         env.reward = 0
         CPRL.set_before_next_decision_reward!(env, model)
         @test env.reward == 3
-    end
-    @testset "set_backtracking_reward!()" begin
-        trailer = CPRL.Trailer()
-        model = CPRL.CPModel(trailer)
-
-        env = CPRL.RLEnv{TestReward}(model)
-
-        env.reward = 0
-        CPRL.set_backtracking_reward!(env, model, :status)
-        @test env.reward == 5
-    end
-    @testset "set_after_decision_reward!()" begin
-        trailer = CPRL.Trailer()
-        model = CPRL.CPModel(trailer)
-
-        env = CPRL.RLEnv{TestReward}(model)
-
-        env.reward = 5
-        CPRL.set_after_decision_reward!(env, model)
-        @test env.reward == 18
     end
     @testset "set_final_reward!()" begin
         trailer = CPRL.Trailer()
