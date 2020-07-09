b0d3d3e3892639080b5f4ae41c18c705fbb9e660
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-07-09 10:05:16 +0200

    WIP: making previous branch commits work

--- src/RL/env/reward.jl
+++ src/RL/env/reward.jl
@@ -6,6 +6,16 @@ This is the default reward, that will be used if no custom reward is specified w
 struct DefaultReward <: AbstractReward end
 
 """
+    set_reward!(env::RLEnv, model::CPModel, symbol::Union{Nothing, Symbol})
+
+Change the "reward" attribute of the env. This is compulsory as used in the buffer
+for the training.
+"""
+function set_reward!(env::RLEnv{DefaultReward}, model::CPModel, symbol::Union{Nothing, Symbol})
+    nothing
+end
+
+"""
     set_before_next_decision_reward!(env::RLEnv{DefaultReward}, model::CPModel)
 
 Change the reward of `env`. This is called right before making the next decision, so you know you have the very last state before the new decision
@@ -18,11 +28,11 @@ end
 
 
 """
-    set_final_reward!(env::RLEnv{DefaultReward}, model::CPModel)
+    set_final_reward!(env::RLEnv{DefaultReward}, model::CPModel, symbol::Union{Nothing, Symbol})
 
 Change the "reward" attribute of the env. Called when the optimality is proved.
 """
-function set_final_reward!(env::RLEnv{DefaultReward}, model::CPModel)
+function set_final_reward!(env::RLEnv{DefaultReward}, model::CPModel, symbol::Union{Nothing, Symbol})
     env.reward += 30/(model.statistics.numberOfNodes)
     nothing
 end
