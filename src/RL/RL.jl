f66e527c1062f4d516cdd51095349941e532f1b5
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-07-16 18:48:54 +0200

    delete RLEnv

--- src/RL/RL.jl
+++ src/RL/RL.jl
@@ -3,7 +3,6 @@ using GeometricFlux
 
 
 include("representation/representation.jl")
-include("env/env.jl")
 include("nn_structures/nn_structures.jl")
 include("learners/cpdqn.jl")
 include("explorer/cp_explorer.jl")
