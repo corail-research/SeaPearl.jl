ebb85d2eb5d53addb208478d655e7046fae07dee
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-07-16 18:49:23 +0200

    merge RLEnv in LearnedHeuristic

--- src/CP/valueselection/learnedheuristic.jl
+++ src/CP/valueselection/learnedheuristic.jl
@@ -1,15 +1,32 @@
 
 include("searchmetrics.jl")
 
+"""
+    abstract type AbstractReward end
+
+Used to customize the reward function. If you want to use your own reward, you have to create a struct
+(called `CustomReward` for example) and define the following methods:
+- set_reward!(::StepPhase, env::RLEnv{CustomReward}, model::CPModel, current_status::Union{Nothing, Symbol})
+- set_reward!(::DecisionPhase, env::RLEnv{CustomReward}, model::CPModel)
+- set_reward!(::EndingPhase, env::RLEnv{CustomReward}, symbol::Symbol)
+
+Then, when creating the `LearnedHeuristic`, you define it using `LearnedHeuristic{CustomReward}(agent::RL.Agent)`
+and your functions will be called instead of the default ones.
+"""  
+
+abstract type AbstractReward end
+
 mutable struct LearnedHeuristic{R<:AbstractReward, A<:ActionOutput} <: ValueSelection
     agent::RL.Agent
     fitted_problem::Union{Nothing, Type{G}} where G
     fitted_strategy::Union{Nothing, Type{S}} where S <: SearchStrategy
-    current_env::Union{Nothing, RLEnv}
+    action_space::Union{Nothing, RL.DiscreteSpace{Array{Int64,1}}}
+    current_state::Union{Nothing, CPGraph}
+    current_reward::Union{Nothing, Float64}
     cpnodes_max::Union{Nothing, Int64}
-    search_metrics::SearchMetrics
+    search_metrics::Union{Nothing, SearchMetrics}
 
-    LearnedHeuristic{R, A}(agent::RL.Agent, cpnodes_max=nothing) where {R, A}= new{R, A}(agent, nothing, nothing, nothing, cpnodes_max)
+    LearnedHeuristic{R, A}(agent::RL.Agent, cpnodes_max=nothing) where {R, A}= new{R, A}(agent, nothing, nothing, nothing, nothing, nothing, cpnodes_max, nothing)
 end
 
 LearnedHeuristic(agent::RL.Agent) = LearnedHeuristic{DefaultReward, FixedOutput}(agent)
@@ -18,6 +35,59 @@ Flux.testmode!(lh::LearnedHeuristic, mode = true) = Flux.testmode!(lh.agent, mod
 
 
 ######### UTILS
+
+function update_with_cpmodel!(lh::LearnedHeuristic, model::CPModel)
+    if isnothing(model.RLRep)
+        model.RLRep = CPLayerGraph(model)
+    end
+    # construct the action_space
+    variables = collect(values(model.variables))
+    valuesOfVariables = sort(arrayOfEveryValue(variables))
+
+    lh.action_space = RL.DiscreteSpace(valuesOfVariables)
+    lh.current_state = CPGraph(model, 0)
+    lh.current_reward = 0
+    lh.search_metrics = SearchMetrics(model)
+
+    lh
+end
+
+include("reward.jl")
+
+"""
+    sync!(lh::LearnedHeuristic, model::CPModel, x::AbstractIntVar)
+
+Synchronize the env with the CPModel.
+"""
+function sync_state!(lh::LearnedHeuristic, model::CPModel, x::AbstractIntVar)
+    if isnothing(model.RLRep)
+        model.RLRep = CPLayerGraph(model)
+    end
+    update_graph!(lh.current_state, model.RLRep, x)
+    nothing 
+end
+
+function get_observation!(lh::LearnedHeuristic, model::CPModel, x::AbstractIntVar, done = false)
+    # get legal_actions_mask
+    legal_actions_mask = [value in x.domain for value in lh.action_space]
+
+    # compute legal actions
+    legal_actions = lh.action_space.span[legal_actions_mask]
+
+    reward = lh.current_reward
+    # Initialize reward for the next state: not compulsory with DefaultReward, but maybe useful in case the user forgets it
+    lh.current_reward = 0
+
+    # synchronize state: we could delete env.state, we do not need it 
+    sync_state!(lh, model, x)
+
+    state = to_array(lh.current_state, lh.cpnodes_max)
+    state = reshape(state, size(state)..., 1)
+    # println("reward", reward)
+    
+    # return the observation as a named tuple (useful for interface understanding)
+    return (reward = reward, terminal = done, state = state, legal_actions = legal_actions, legal_actions_mask = legal_actions_mask)
+end
 
 """
     set_metrics!(PHASE::T, lh::LearnedHeuristic, model::CPModel, symbol::Union{Nothing, Symbol}, x::Union{Nothing, AbstractIntVar}) where T <: LearningPhase 
@@ -31,15 +101,6 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
 ###### LEARNINGPHASEs BEHAVIOUR
 
 """
@@ -50,15 +111,14 @@ the pre episode stage (basically making sure that the buffer is empty).
 """
 function (valueSelection::LearnedHeuristic{R})(::InitializingPhase, model::CPModel, x::Union{Nothing, AbstractIntVar}, current_status::Union{Nothing, Symbol}) where R<:AbstractReward
     # create the environment
-    valueSelection.current_env = RLEnv{R}(model::CPModel; cpnodes_max=valueSelection.cpnodes_max)
+    update_with_cpmodel!(valueSelection, model)
     false_x = first(values(model.variables))
-    obs = observe!(valueSelection.current_env, model, false_x)
+    obs = get_observation!(valueSelection, model, false_x)
 
     # Reset the agent, useful for things like recurrent networks
     Flux.reset!(valueSelection.agent)
 
     valueSelection.agent(RL.PRE_EPISODE_STAGE, obs) # just empty the buffer
-    # eventually hook(PRE_EPISODE_STAGE, agent, env, obs)
 end
 
 """
@@ -70,9 +130,9 @@ ATM, the metrics are updated after the reward assignment as the current_status g
 Another possibility would be to have old and new metrics in memory. 
 """
 function (valueSelection::LearnedHeuristic)(PHASE::StepPhase, model::CPModel, x::Union{Nothing, AbstractIntVar}, current_status::Union{Nothing, Symbol})
-    set_reward!(PHASE, valueSelection.current_env, model, current_status)
+    set_reward!(PHASE, valueSelection, model, current_status)
     # incremental metrics, set after reward is updated
-    set_metrics!(PHASE, valueSelection.current_env, model, current_status, nothing)
+    set_metrics!(PHASE, valueSelection, model, current_status, nothing)
     nothing
 end
 
@@ -86,10 +146,10 @@ be used in the other set_reward! function as the reward of the last action will 
 """
 function (valueSelection::LearnedHeuristic)(PHASE::DecisionPhase, model::CPModel, x::Union{Nothing, AbstractIntVar}, current_status::Union{Nothing, Symbol})
     # domain change metrics, set before reward is updated
-    set_metrics!(PHASE, valueSelection.current_env, model, nothing, x)
-    set_reward!(PHASE, valueSelection.current_env, model)
+    set_metrics!(PHASE, valueSelection, model, nothing, x)
+    set_reward!(PHASE, valueSelection, model)
 
-    obs = observe!(valueSelection.current_env, model, x)
+    obs = get_observation!(valueSelection, model, x)
 
     if !wears_mask(valueSelection)
         obs = (reward = obs.reward, terminal = obs.terminal, state = obs.state)
@@ -98,14 +158,12 @@ function (valueSelection::LearnedHeuristic)(PHASE::DecisionPhase, model::CPModel
     #println("Decision  ", obs.reward, " ", obs.terminal, " ", obs.legal_actions, " ", obs.legal_actions_mask)
     if model.statistics.numberOfNodes > 1
         valueSelection.agent(RL.POST_ACT_STAGE, obs) # get terminal and reward
-        # eventually: hook(POST_ACT_STAGE, agent, env, obs, action)
     end
     action = valueSelection.agent(RL.PRE_ACT_STAGE, obs) # choose action, store it with the state
     
     return action_to_value(valueSelection, action, obs.state, model)
     # println("Assign value : ", cp_vertex.value, " to variable : ", x)
     
-    # set_after_decision_reward!(valueSelection.current_env, model)
 end
 
 function from_order_to_id(state::AbstractArray, value_order::Int64)
@@ -133,20 +191,17 @@ Set the final reward, do last observation.
 """
 function (valueSelection::LearnedHeuristic)(PHASE::EndingPhase, model::CPModel, x::Union{Nothing, AbstractIntVar}, current_status::Union{Nothing, Symbol})
     # the RL EPISODE stops
-    set_done!(valueSelection.current_env, true)
-    set_reward!(PHASE, valueSelection.current_env, model, current_status)
+    set_reward!(PHASE, valueSelection, model, current_status)
     false_x = first(values(model.variables))
-    obs = observe!(valueSelection.current_env, model, false_x)
+    obs = get_observation!(valueSelection, model, false_x, true)
     if !wears_mask(valueSelection)
         obs = (reward = obs.reward, terminal = obs.terminal, state = obs.state)
     end
     #println("EndingPhase  ", obs.reward, " ", obs.terminal, " ", obs.legal_actions, " ", obs.legal_actions_mask)
 
     valueSelection.agent(RL.POST_ACT_STAGE, obs) # get terminal and reward
-    # eventually: hook(POST_ACT_STAGE, agent, env, obs, action)
 
     valueSelection.agent(RL.POST_EPISODE_STAGE, obs)  # let the agent see the last observation
-    # eventually hook(POST_EPISODE_STAGE, agent, env, obs)
 end
 
 wears_mask(valueSelection::LearnedHeuristic) = wears_mask(valueSelection.agent.policy.learner.approximator.model)
