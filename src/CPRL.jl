5ccaba8cb7bd0b49498903029c4cf9b0fbe9ef23
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-07-17 16:18:00 +0200

    delete three unuseful lines

--- src/CPRL.jl
+++ src/CPRL.jl
@@ -9,13 +9,6 @@ include("MOI_wrapper/MOI_wrapper.jl")
 include("datagen/datagen.jl")
 include("experiment/experiment.jl")
 
-(app::RL.NeuralNetworkApproximator)(obs::NamedTuple{(:reward, :terminal, :state, :legal_actions, :legal_actions_mask)}) = app.model(obs)
-(app::RL.NeuralNetworkApproximator)(state::CPGraph) = app.model(state)
-
-(app::RL.NeuralNetworkApproximator)(state::CPGraph, a::Int) = app.model(state)[a]
-
-
-
 greet() = print("Hello World!")
 
 export
