2cee2feb09c9bce710272020dc891de5883284be
Ilan Coulon <ilan.coulon@gmail.com>
2020-07-20 16:00:08 +0200

    Add custom featurization test

--- test/RL/representation/representation.jl
+++ test/RL/representation/representation.jl
@@ -1,15 +1,23 @@
 struct UnimplementedFeaturization <: CPRL.AbstractFeaturization end
-struct UnimplementFeatState{F} <: CPRL.FeaturizedStateRepresentation{F} end
+struct TestFeatState{F} <: CPRL.FeaturizedStateRepresentation{F} end
 
+struct TestFeaturization <: CPRL.AbstractFeaturization end
 
+function CPRL.featurize(::TestFeatState{TestFeaturization})
+    return [1, 2, 3]
+end
 
 @testset "representation.jl" begin
 
-    @testset "Unimplemented featurization" begin
-        @test_throws ErrorException CPRL.featurize(UnimplementFeatState{UnimplementedFeaturization}())
-    end
-
     include("default/cp_layer/cp_layer.jl")
     include("default/defaultstaterepresentation.jl")
+
+    @testset "Unimplemented featurization" begin
+        @test_throws ErrorException CPRL.featurize(TestFeatState{UnimplementedFeaturization}())
+    end
+
+    @testset "Custom featurization" begin
+        @test CPRL.featurize(TestFeatState{TestFeaturization}()) == [1, 2, 3]
+    end
 
 end
