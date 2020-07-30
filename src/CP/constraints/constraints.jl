d24d59800b9d7c9a530f3ebe6313fe36a73fa71a
Felix Chalumeau <felix.chalumeau@hotmail.fr>
2020-07-30 11:56:18 +0200

    add file to contraints

--- src/CP/constraints/constraints.jl
+++ src/CP/constraints/constraints.jl
@@ -8,6 +8,7 @@ include("sumtozero.jl")
 include("sumlessthan.jl")
 include("sumgreaterthan.jl")
 include("islessorequal.jl")
+include("isbinaryor.jl")
 
 """
     addOnDomainChange!(x::AbstractIntVar, constraint::Constraint)
