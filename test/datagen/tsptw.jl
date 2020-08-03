0566fa13982de0ae70879e1729546623f380a659
Ilan Coulon <ilan.coulon@gmail.com>
2020-08-03 15:28:43 +0200

    TSPTW: more precise tests

--- test/datagen/tsptw.jl
+++ test/datagen/tsptw.jl
@@ -10,13 +10,31 @@
 
         generator = SeaPearl.TsptwGenerator(n_city, grid_size, max_tw_gap, max_tw)
 
-        for _ in 1:3
-            SeaPearl.fill_with_generator!(model, generator)
+        SeaPearl.fill_with_generator!(model, generator; seed=52)
 
-            @test length(keys(model.variables)) == nb_items + 3
-            @test length(model.constraints) == 3
-            empty!(model)
-        end
+        @test length(keys(model.variables)) == 2 * n_city^2 + 10 * n_city + 1
+        @test length(model.constraints) == 4 + 6*(n_city-1) + 2*n_city + 3*n_city^2
+        empty!(model)
     end
+    @testset "Search instance" begin
+        trailer = SeaPearl.Trailer()
+        model = SeaPearl.CPModel(trailer)
 
+        n_city = 3
+        grid_size = 40
+        max_tw_gap = 5
+        max_tw = 10
+
+        generator = SeaPearl.TsptwGenerator(n_city, grid_size, max_tw_gap, max_tw)
+
+        SeaPearl.fill_with_generator!(model, generator; seed=52)
+
+        variableheuristic = SeaPearl.MinDomainVariableSelection{false}()
+        my_heuristic(x::SeaPearl.IntVar) = minimum(x.domain)
+        valueheuristic = SeaPearl.BasicHeuristic(my_heuristic)
+
+        SeaPearl.search!(model, SeaPearl.DFSearch, variableheuristic, valueheuristic)
+
+        @test model.solutions == []
+    end
 end
