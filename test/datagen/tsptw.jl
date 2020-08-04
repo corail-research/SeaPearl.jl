e46579dbde0c19e17808e8c4025d2dd42bcd79b2
Ilan Coulon <ilan.coulon@gmail.com>
2020-08-04 15:05:01 +0200

    TSPTW: more precise tests

--- test/datagen/tsptw.jl
+++ test/datagen/tsptw.jl
@@ -17,9 +17,9 @@ end
         model = SeaPearl.CPModel(trailer)
 
         n_city = 10
-        grid_size = 10
-        max_tw_gap = 5
-        max_tw = 10
+        grid_size = 5
+        max_tw_gap = 1
+        max_tw = 1
 
         generator = SeaPearl.TsptwGenerator(n_city, grid_size, max_tw_gap, max_tw)
 
@@ -33,40 +33,41 @@ end
         trailer = SeaPearl.Trailer()
         model = SeaPearl.CPModel(trailer)
 
-        n_city = 10
-        grid_size = 40
-        max_tw_gap = 5
-        max_tw = 10
+        n_city = 5
+        grid_size = 150
+        max_tw_gap = 3
+        max_tw = 3
 
         generator = SeaPearl.TsptwGenerator(n_city, grid_size, max_tw_gap, max_tw)
 
-        dist, time_windows, x_pos, y_pos = SeaPearl.fill_with_generator!(model, generator; seed=52)
+        dist, time_windows, x_pos, y_pos = SeaPearl.fill_with_generator!(model, generator; seed=55)
 
         variableheuristic = TsptwVariableSelection{false}()
         my_heuristic(x::SeaPearl.IntVar) = minimum(x.domain)
         valueheuristic = SeaPearl.BasicHeuristic(my_heuristic)
 
-        # println("model", model)
-        println()
-        println()
-        println()
-        println()
         SeaPearl.search!(model, SeaPearl.DFSearch, variableheuristic, valueheuristic)
-        # println("model", model)
 
-        println("dist", dist)
-        println("time_windows", time_windows)
-        println("x_pos", x_pos)
-        println("y_pos", y_pos)
+        # println("dist", dist)
+        # println("time_windows", time_windows)
 
-        
+        @test dist == [0 47 37 119 74; 47 0 73 148 120; 37 73 0 82 55; 119 148 82 0 85; 74 120 55 85 0]
+        @test time_windows == [0 10; 330 333; 37 37; 181 182; 95 98]
 
-        @test length(model.solutions) >= 1
+        @test model.solutions[end]["a_1"] == 3
+        @test model.solutions[end]["a_2"] == 5
+        @test model.solutions[end]["a_3"] == 4
+        @test model.solutions[end]["a_4"] == 2
+        @test model.solutions[end]["c_5"] == 325
 
-        # println("model.solutions", model.solutions)
-        for i in 1:(n_city-1)
-            println("a_"*string(i)*": ", model.solutions[end]["a_"*string(i)])
-        end
-        println("c_"*string(n_city)*": ", model.solutions[end]["c_"*string(n_city)])
+        @test length(model.solutions) == 1
+
+        println("nodes: ", model.statistics.numberOfNodes)
+
+        # # println("model.solutions", model.solutions)
+        # for i in 1:(n_city-1)
+        #     println("a_"*string(i)*": ", model.solutions[end]["a_"*string(i)])
+        # end
+        # println("c_"*string(n_city)*": ", model.solutions[end]["c_"*string(n_city)])
     end
 end
