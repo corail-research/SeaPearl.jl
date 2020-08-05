89062e4ee76de63ba6550aacf4b58b975038f5d5
Ilan Coulon <ilan.coulon@gmail.com>
2020-08-05 12:21:01 +0200

    Test branchable variables

--- test/CP/core/model.jl
+++ test/CP/core/model.jl
@@ -10,10 +10,37 @@
         SeaPearl.addVariable!(model, y)
 
         @test length(model.variables) == 2
+        @test SeaPearl.branchable_variables(model) == [x, y]
 
         z = SeaPearl.IntVar(2, 6, "y", trailer)
 
         @test_throws AssertionError SeaPearl.addVariable!(model, z)
+
+        # Not branching
+        trailer = SeaPearl.Trailer()
+        x = SeaPearl.IntVar(2, 6, "x", trailer)
+        y = SeaPearl.IntVar(2, 6, "y", trailer)
+
+        model = SeaPearl.CPModel(trailer)
+
+        SeaPearl.addVariable!(model, x; branchable=false)
+        SeaPearl.addVariable!(model, y)
+
+        @test length(model.variables) == 2
+        @test SeaPearl.branchable_variables(model) == [y]
+
+        # Trying to branch on Set variable
+        trailer = SeaPearl.Trailer()
+        x = SeaPearl.IntVar(2, 6, "x", trailer)
+        y = SeaPearl.IntSetVar(2, 6, "y", trailer)
+
+        model = SeaPearl.CPModel(trailer)
+
+        SeaPearl.addVariable!(model, x; branchable=false)
+        @test_throws AssertionError SeaPearl.addVariable!(model, y)
+
+        @test length(model.variables) == 2
+        @test SeaPearl.branchable_variables(model) == [y]
     end
 
     @testset "merge!()" begin
