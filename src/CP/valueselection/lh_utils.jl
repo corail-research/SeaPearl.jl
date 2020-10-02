4f74c8330033adcde47fee558a4e026975da1a03
Ilan Coulon <ilan.coulon@gmail.com>
2020-10-02 10:21:36 +0100

    Indicate ActionStyle of CPEnv

--- src/CP/valueselection/lh_utils.jl
+++ src/CP/valueselection/lh_utils.jl
@@ -78,9 +78,24 @@ function get_observation!(lh::LearnedHeuristic, model::CPModel, x::AbstractIntVa
     # println("reward", reward)
     
     # return the observation as a named tuple (useful for interface understanding)
-    return (reward = reward, terminal = done, state = state, legal_actions = legal_actions, legal_actions_mask = legal_actions_mask)
+    return CPEnv(reward, done, state, legal_actions, legal_actions_mask)
 end
 
+struct CPEnv <: AbstractEnv
+    reward
+    terminal
+    state
+    legal_actions
+    legal_actions_mask
+end
+
+RLBase.get_actions(env::CPEnv) = env.legal_actions
+RLBase.get_legal_actions(env::CPEnv) = env.legal_actions
+RLBase.get_legal_actions_mask(env::CPEnv) = env.legal_actions_mask
+RLBase.get_reward(env::CPEnv) = env.reward
+RLBase.get_terminal(env::CPEnv) = env.terminal
+RLBase.get_state(env::CPEnv) = env.state
+ActionStyle(CPEnv) = FULL_ACTION_SET
 """
     set_metrics!(PHASE::T, lh::LearnedHeuristic, model::CPModel, symbol::Union{Nothing, Symbol}, x::Union{Nothing, AbstractIntVar}) where T <: LearningPhase 
 
