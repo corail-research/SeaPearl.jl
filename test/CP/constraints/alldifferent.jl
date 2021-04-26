a48c4782bc6b96e9d0b86f32cc826cc095aeff03
PierreTsr <55246205+PierreTsr@users.noreply.github.com>
2021-04-26 12:28:59 -0400

    added 5-queens and 7-queens => check no solution is discarded

--- test/CP/constraints/alldifferent.jl
+++ test/CP/constraints/alldifferent.jl
@@ -279,4 +279,70 @@
             @test !SeaPearl.propagate!(con2, toPropagate, modif)
         end
     end
+    @testset "5 queens full" begin
+        n=5
+        trailer = SeaPearl.Trailer()
+        model = SeaPearl.CPModel(trailer)
+
+        rows = Vector{SeaPearl.AbstractIntVar}(undef, n)
+        for i = 1:n
+            rows[i] = SeaPearl.IntVar(1, n, "row_"*string(i), trailer)
+            SeaPearl.addVariable!(model, rows[i]; branchable=true)
+        end
+
+        rows_plus = Vector{SeaPearl.AbstractIntVar}(undef, n)
+        for i = 1:n
+            rows_plus[i] = SeaPearl.IntVarViewOffset(rows[i], i, rows[i].id*"+"*string(i))
+            #SeaPearl.addVariable!(model, rows_plus[i]; branchable=false)
+        end
+
+        rows_minus = Vector{SeaPearl.AbstractIntVar}(undef, n)
+        for i = 1:n
+            rows_minus[i] = SeaPearl.IntVarViewOffset(rows[i], -i, rows[i].id*"-"*string(i))
+            #SeaPearl.addVariable!(model, rows_minus[i]; branchable=false)
+        end
+
+        push!(model.constraints, SeaPearl.AllDifferent(rows, trailer))
+        push!(model.constraints, SeaPearl.AllDifferent(rows_plus, trailer))
+        push!(model.constraints, SeaPearl.AllDifferent(rows_minus, trailer))
+
+        variableSelection = SeaPearl.MinDomainVariableSelection{false}()
+        status = @time SeaPearl.solve!(model; variableHeuristic=variableSelection)
+
+        @test status == :Optimal
+        @test length(model.solutions) == 10
+    end
+    @testset "7 queens full" begin
+        n=7
+        trailer = SeaPearl.Trailer()
+        model = SeaPearl.CPModel(trailer)
+
+        rows = Vector{SeaPearl.AbstractIntVar}(undef, n)
+        for i = 1:n
+            rows[i] = SeaPearl.IntVar(1, n, "row_"*string(i), trailer)
+            SeaPearl.addVariable!(model, rows[i]; branchable=true)
+        end
+
+        rows_plus = Vector{SeaPearl.AbstractIntVar}(undef, n)
+        for i = 1:n
+            rows_plus[i] = SeaPearl.IntVarViewOffset(rows[i], i, rows[i].id*"+"*string(i))
+            #SeaPearl.addVariable!(model, rows_plus[i]; branchable=false)
+        end
+
+        rows_minus = Vector{SeaPearl.AbstractIntVar}(undef, n)
+        for i = 1:n
+            rows_minus[i] = SeaPearl.IntVarViewOffset(rows[i], -i, rows[i].id*"-"*string(i))
+            #SeaPearl.addVariable!(model, rows_minus[i]; branchable=false)
+        end
+
+        push!(model.constraints, SeaPearl.AllDifferent(rows, trailer))
+        push!(model.constraints, SeaPearl.AllDifferent(rows_plus, trailer))
+        push!(model.constraints, SeaPearl.AllDifferent(rows_minus, trailer))
+
+        variableSelection = SeaPearl.MinDomainVariableSelection{false}()
+        status = @time SeaPearl.solve!(model; variableHeuristic=variableSelection)
+
+        @test status == :Optimal
+        @test length(model.solutions) == 40
+    end
 end
