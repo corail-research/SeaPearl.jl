0c525156fe71c4d0af33aa39f1fecc3259ad693c
Tom <tom.marty@polytechnique.edu>
2021-05-03 13:07:55 -0400

    removed dead code

--- src/RL/RL.jl
+++ src/RL/RL.jl
@@ -3,6 +3,4 @@ using GeometricFlux
 include("representation/representation.jl")
 include("nn_structures/nn_structures.jl")
 include("learners/cpdqn.jl")
-# include("explorer/cp_explorer.jl")
-# include("explorer/directed_explorer.jl")
 include("utils.jl")
