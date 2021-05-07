f02e2047597444cb1bcdfadabfa55f4fc6babebf
PierreTsr <55246205+PierreTsr@users.noreply.github.com>
2021-05-07 17:30:21 -0400

    fix testset

--- test/CP/constraints/algorithms/rsparsebitset.jl
+++ test/CP/constraints/algorithms/rsparsebitset.jl
@@ -42,8 +42,8 @@
         SeaPearl.setValue!(set.limit, 6)
 
         SeaPearl.clearMask!(set)
-        @test set.mask[1:6] .= UInt64(0)
-        @test set.mask[7:end] .= typemax(UInt64)
+        @test all(set.mask[1:6] .== UInt64(0))
+        @test all(set.mask[7:end] .== typemax(UInt64))
     end
 
     @testset "reverseMask!" begin
@@ -55,10 +55,10 @@
         set.mask[1:3] .= typemax(UInt64)
         SeaPearl.setValue!(set.limit, 6)
 
-        SeaPearl.reverseMask(set)
-        @test set.mask[1:3] .= UInt64(0)
-        @test set.mask[3:6] .= typemax(UInt64)
-        @test set.mask[7:end] .= UInt64(0)
+        SeaPearl.reverseMask!(set)
+        @test all(set.mask[1:3] .== UInt64(0))
+        @test all(set.mask[4:6] .== typemax(UInt64))
+        @test all(set.mask[7:end] .== UInt64(0))
     end
 
     @testset "addToMask!" begin
@@ -82,7 +82,7 @@
         @test all(set.mask .== x)
 
         SeaPearl.addToMask!(set, y)
-        @test all(set.masl .== x .| y)
+        @test all(set.mask .== x .| y)
     end
 
     @testset "intersectWithMask!" begin
@@ -97,10 +97,11 @@
             UInt8(1)
         ]
         
+        SeaPearl.intersectWithMask!(set)
         @test set[1]
         @test set[6]
         @test set[12]
-        @test !any(set[setdiff(1:24, [1,6,12])])
+        @test !any([set[i] for i in setdiff(1:24, [1,6,12])])
     end
 
     @testset "intersectIndex" begin
@@ -112,7 +113,7 @@
         set.mask[1:end] = [
             UInt8(1) << 7 | UInt8(1) << 2, 
             UInt8(1) << 4,
-            UInt8(1)
+            UInt8(1) << 7
         ]
         SeaPearl.intersectWithMask!(set)
 
