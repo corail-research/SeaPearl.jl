23023c6aa8d52f89d93e24ed7cb43531022cb998
Tom <tom.marty@polytechnique.edu>
2021-05-18 12:20:54 -0400

    added iLDS search strategy

--- src/CP/core/search/ilds.jl
+++ src/CP/core/search/ilds.jl
@@ -1,22 +1,17 @@
 
-"""
-initroot!(toCall::Stack{Function}, ::Type{ILDSearch},model::CPModel, variableHeuristic::AbstractVariableSelection, valueSelection::ValueSelection, newConstraints=nothing)
-
-Used as a generic function to instantiate the research based on a specific Strategy <: SearchStrategy. 
-"""
 function initroot!(toCall::Stack{Function}, ::Type{ILDSearch}, model::CPModel, variableHeuristic::AbstractVariableSelection, valueSelection::ValueSelection, newConstraints=nothing)
+    isboundedlist = [!isbound(v) for (k,v) in model.variables]
+    @assert !isempty(isboundedlist) "initialisation failed : no declared variables"
+    depth = sum(isboundedlist)
     
-    depth = sum([v for (k,v)in model.branchable])
-    println("hauteur de l'arbre de recherche : ", depth )
-    
-    for k in range depth:-1:1
-        push!(toCall, (model) -> (;expandIlds!(toCall,k,depth, model, variableHeuristic, valueSelection)))
+    for k in depth:-1:1
+        push!(toCall, (model) -> (nothing;expandIlds!(toCall,k,depth, nothing, model, variableHeuristic, valueSelection)))
     end
-    return expandIlds!(toCall,0,depth, model, variableHeuristic, valueSelection)
+    return expandIlds!(toCall,0,depth, nothing, model, variableHeuristic, valueSelection)
 end
 
 
-function expandIlds!(toCall::Stack{Function}, discrepancy::Int64, depth::Int64, model::CPModel, variableHeuristic::AbstractVariableSelection, valueSelection::ValueSelection, newConstraints=nothing)
+function expandIlds!(toCall::Stack{Function}, discrepancy::Int64, previousdepth::Int64, direction::Union{Nothing, Symbol} , model::CPModel, variableHeuristic::AbstractVariableSelection, valueSelection::ValueSelection, newConstraints=nothing)
     # Dealing with limits
     model.statistics.numberOfNodes += 1
     if !belowNodeLimit(model)
@@ -32,28 +27,31 @@ function expandIlds!(toCall::Stack{Function}, discrepancy::Int64, depth::Int64, 
         return :Infeasible
     end
     if solutionFound(model)
-        triggerFoundSolution!(model)
+        if (direction ==:Left && discrepancy == 0)  || (direction ==:Right && discrepancy == 1 ) || isnothing(direction)
+            triggerFoundSolution!(model) 
+        end
         return :FoundSolution 
     end
-
+    depth = sum([!isbound(v) for (k,v) in model.variables])  #recomputed at each step, maybe not efficient
+    discrepancy =(depth != previousdepth && direction == :Right ) ? discrepancy - 1 : discrepancy
+    @assert depth > 0 "a least one variable should be branchable"
     # Variable selection
     x = variableHeuristic(model)
 
     # Value selection
     v = valueSelection(DecisionPhase(), model, x, nothing)
-    #println("Value : ", v, " assigned to : ", x.id)
 
-    if (discrepancy>0)
+    if (discrepancy>=0)   
         push!(toCall, (model) -> (restoreState!(model.trailer); :BackTracking))
-        push!(toCall, (model) -> (assign!(x, rand(x.domain.values)); expandILDS!(toCall,discrepancy-1, depth-1, model, variableHeuristic, valueSelection, getOnDomainChange(x))))
+        push!(toCall, (model) -> (remove!(x.domain, v); expandIlds!(toCall,discrepancy, depth, :Right, model, variableHeuristic, valueSelection, getOnDomainChange(x))))
         push!(toCall, (model) -> (saveState!(model.trailer); :SavingState))
     end
 
     if (depth>discrepancy)
         push!(toCall, (model) -> (restoreState!(model.trailer); :BackTracking))
-        push!(toCall, (model) -> (assign!(x, v); expandILDS!(toCall,discrepancy, depth-1, model, variableHeuristic, valueSelection, getOnDomainChange(x))))
+        push!(toCall, (model) -> (assign!(x, v); expandIlds!(toCall,discrepancy, depth-1, :Left, model, variableHeuristic, valueSelection, getOnDomainChange(x))))
         push!(toCall, (model) -> (saveState!(model.trailer); :SavingState))
     end
-
     return :Feasible
 end
+
