2b93b85c0af5712c116fc4ad86fdb4cc0427013f
Tom <tom.marty@polytechnique.edu>
2021-05-26 11:51:43 -0400

    added struct AbstractTakeObjective

--- src/experiment/metrics/metrics.jl
+++ src/experiment/metrics/metrics.jl
@@ -1,3 +1,16 @@
 abstract type AbstractMetrics end
 
+abstract type AbstractTakeObjective end 
+
+struct TakeObjective<:AbstractTakeObjective end
+struct DontTakeObjective<:AbstractTakeObjective end
+
+
+
 include("basicmetrics.jl")
+
+#TODO implement this function
+"""function plot_diff_metrics(...)
+    
+end
+"""
