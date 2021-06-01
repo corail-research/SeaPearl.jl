c16605fca43b84ac5f76aa9cf0b20e5084c4f83b
PierreTsr <55246205+PierreTsr@users.noreply.github.com>
2021-06-01 14:35:50 -0400

    refactoring DefaultStateRepresentation

--- src/RL/representation/default/defaultstaterepresentation.jl
+++ src/RL/representation/default/defaultstaterepresentation.jl
@@ -1,21 +1,20 @@
-
-struct DefaultFeaturization <: AbstractFeaturization end
+using GraphSignals
 
 include("cp_layer/cp_layer.jl")
 
 """
-    DefaultStateRepresentation{F}
+    DefaultStateRepresentation{F, TS}
 
 This is the default representation used by SeaPearl unless the user define his own and give
 the information to his LearnedHeurstic when defining it. 
 """
-mutable struct DefaultStateRepresentation{F} <: FeaturizedStateRepresentation{F}
+mutable struct DefaultStateRepresentation{F, TS} <: FeaturizedStateRepresentation{F, TS}
     cplayergraph::CPLayerGraph
     features::Union{Nothing, AbstractArray{Float32, 2}}
     variable_id::Union{Nothing, Int64}
 end
 
-function DefaultStateRepresentation{F}(model::CPModel) where F
+function DefaultStateRepresentation{F, TS}(model::CPModel) where {F, TS}
     g = CPLayerGraph(model)
     sr = DefaultStateRepresentation{F}(g, nothing, nothing, nothing)
 
@@ -24,10 +23,8 @@ function DefaultStateRepresentation{F}(model::CPModel) where F
     sr
 end
 
-DefaultStateRepresentation(m::CPModel) = DefaultStateRepresentation{DefaultFeaturization}(m::CPModel)
-
 """ 
-        function update_representation!(sr::DefaultStateRepresentation, model::CPModel, x::AbstractIntVar)
+    function update_representation!(sr::DefaultStateRepresentation, model::CPModel, x::AbstractIntVar)
 
 While working with DefaultStateRepresentation, at each step of the research node, only the variable_id need to be updated. 
 features don't need to be updated as they encode the initial problem and the CPLayerGraph is automatically updated as it is linked to the CPModel. 
@@ -36,6 +33,12 @@ function update_representation!(sr::DefaultStateRepresentation, model::CPModel, 
     sr.variable_id = indexFromCpVertex(sr.cplayergraph, VariableVertex(x))
     sr
 end
+
+function trajectoryState(sr::DefaultStateRepresentation{F, TS}) where {F, TS}
+    return TS(sr)
+end
+
+struct DefaultFeaturization <: AbstractFeaturization end
 
 """
     function featurize(sr::DefaultStateRepresentation{DefaultFeaturization})
@@ -68,5 +71,20 @@ Returns the length of the feature vector, useful for SeaPearl to choose the size
 """
 feature_length(gen::SeaPearl.AbstractModelGenerator, ::Type{DefaultStateRepresentation{DefaultFeaturization}}) = 3
 
+struct DefaultTrajectoryState <: AbstractTrajectoryState
+    fg::GraphSignals.FeaturedGraph
+    variabeIdx::Int
+end
+
+function DefaultTrajectoryState(sr::DefaultStateRepresentation{F, DefaultTrajectoryState}) where {F}
+    if isnothing(sr.variable_id)
+        throw(ErrorException("Unable to build a DefaultTrajectoryState, when the branching variable is nothing."))
+    end
+    # TODO: Implement FeaturedGraph(CPLayerGraph; kwargs...) 
+    fg = FeaturedGraph(sr.cplayergraph; nf=sr.features)
+    return DefaultTrajectoryState(fg, sr.variable_id)
+end
+
+DefaultStateRepresentation(m::CPModel) = DefaultStateRepresentation{DefaultFeaturization}(m::CPModel)
 
 
