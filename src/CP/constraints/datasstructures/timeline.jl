6b1d39852b6208d36a8fc1f17e5fa4144f34cee1
Kim Rioux-Paradis <kim.rioux-paradis.1@ulaval.ca>
2021-06-02 10:57:04 -0400

    Add timeline datastructure and tests

---
+++ src/CP/constraints/datasstructures/timeline.jl
@@ -0,0 +1,57 @@
+mutable struct Timeline
+    timePoints::Array{Int}
+    timePointsCapacity::Array{Int}
+    mapTaskIndexTimePoint::Array{Int}
+    lastestDecrementedTimePoint::Int
+    disjointSet::DisjointSet
+    function Timeline(tasks::Array{Task}) 
+        tasksSortedByEst = sort(tasks, by = x-> x.earliestStartingTime.domain.min.value)
+        timePoints = []
+        timePointsCapacity = []
+        mapTaskIndexTimePoint = fill(-1, length(tasks))
+        maxLCT = 0 
+        sumProcessingTime = 0
+        for task in tasksSortedByEst
+            if length(timePoints) == 0 || timePoints[length(timePoints)] != SeaPearl.getEST(task)
+                push!(timePoints, SeaPearl.getEST(task))
+            end
+            mapTaskIndexTimePoint[task.id] = length(timePoints)
+            sumProcessingTime += task.processingTime
+            if (SeaPearl.getLCT(task) > maxLCT)
+                maxLCT = SeaPearl.getLCT(task)
+            end
+        end
+        push!(timePoints, maxLCT + sumProcessingTime)
+        for timePoint in 1:length(timePoints)-1
+            push!(timePointsCapacity, timePoints[timePoint + 1] - timePoints[timePoint])
+        end
+        disjointSet = SeaPearl.DisjointSet(length(timePoints))
+        lastestDecrementedTimePoint = -1
+        return new(timePoints, timePointsCapacity, mapTaskIndexTimePoint, lastestDecrementedTimePoint, disjointSet)
+    end
+end
+
+function scheduleTask(timeline::Timeline, task::Task)
+    timeToSchedule = task.processingTime
+    timePoint = SeaPearl.greatest!(timeline.disjointSet, timeline.mapTaskIndexTimePoint[task.id])
+    while timeToSchedule > 0
+        timeToScheduleAtThatTimePoint = min(timeline.timePointsCapacity[timePoint], timeToSchedule)
+        timeToSchedule -= timeToScheduleAtThatTimePoint
+        timeline.timePointsCapacity[timePoint] -= timeToScheduleAtThatTimePoint
+        if timeline.timePointsCapacity[timePoint] == 0
+            representative1 = SeaPearl.findRepresentative!(timeline.disjointSet, timePoint)
+            representative2 = SeaPearl.findRepresentative!(timeline.disjointSet, timePoint + 1)
+            SeaPearl.setUnion!(timeline.disjointSet, representative1, representative2)
+            timePoint = SeaPearl.greatest!(timeline.disjointSet, timePoint)
+        end
+    end
+    timeline.lastestDecrementedTimePoint = max(timeline.lastestDecrementedTimePoint, timePoint)
+    return timeline.timePoints[timePoint + 1] - timeline.timePointsCapacity[timePoint]
+end
+
+function earliestCompletionTime(timeline::Timeline)
+    if timeline.lastestDecrementedTimePoint >= 0
+        return timeline.timePoints[timeline.lastestDecrementedTimePoint + 1] - timeline.timePointsCapacity[timeline.lastestDecrementedTimePoint]
+    end
+    return -1
+end
---
+++ test/CP/constraints/datasstructures/timeline.jl
@@ -0,0 +1,134 @@
+@testset "timeline.jl" begin
+    @testset "Timeline(tasks::Task)" begin
+        trailer = SeaPearl.Trailer()
+        startingTime1 = SeaPearl.IntVar(1, 3,"t1", trailer)
+        startingTime2 = SeaPearl.IntVar(2, 4,"t1", trailer)
+        startingTime3 = SeaPearl.IntVar(2, 5,"t1", trailer)
+        task1 = SeaPearl.Task(startingTime1, 1, 1)
+        task2 = SeaPearl.Task(startingTime2, 2, 2)
+        task3 = SeaPearl.Task(startingTime3, 1, 3)
+        tasks = [task1, task2, task3]
+        timeline = SeaPearl.Timeline(tasks)
+
+        @test length(timeline.timePoints) == 3
+        @test timeline.timePoints[1] == 1
+        @test timeline.timePoints[2] == 2
+        @test timeline.timePoints[3] == 10
+
+        @test length(timeline.timePointsCapacity) == 2
+        @test timeline.timePointsCapacity[1] == 1
+        @test timeline.timePointsCapacity[2] == 8
+
+        @test length(timeline.mapTaskIndexTimePoint) == 3
+        @test timeline.mapTaskIndexTimePoint[1] == 1
+        @test timeline.mapTaskIndexTimePoint[2] == 2
+        @test timeline.mapTaskIndexTimePoint[3] == 2
+    end
+
+    @testset "scheduleTask(timeline::Timeline, task::Task)" begin
+        trailer = SeaPearl.Trailer()
+        startingTime1 = SeaPearl.IntVar(1, 3,"t1", trailer)
+        startingTime2 = SeaPearl.IntVar(2, 4,"t1", trailer)
+        startingTime3 = SeaPearl.IntVar(2, 5,"t1", trailer)
+        task1 = SeaPearl.Task(startingTime1, 1, 1)
+        task2 = SeaPearl.Task(startingTime2, 2, 2)
+        task3 = SeaPearl.Task(startingTime3, 1, 3)
+        tasks = [task1, task2, task3]
+        timeline = SeaPearl.Timeline(tasks)
+
+        SeaPearl.scheduleTask(timeline, task1)
+        @test timeline.timePointsCapacity[1] == 0
+        @test timeline.timePointsCapacity[2] == 8
+        @test SeaPearl.findRepresentative!(timeline.disjointSet, 1) == SeaPearl.findRepresentative!(timeline.disjointSet, 2)
+        @test timeline.lastestDecrementedTimePoint == 2
+
+        SeaPearl.scheduleTask(timeline, task2)
+        @test timeline.timePointsCapacity[1] == 0
+        @test timeline.timePointsCapacity[2] == 6
+        @test SeaPearl.findRepresentative!(timeline.disjointSet, 1) == SeaPearl.findRepresentative!(timeline.disjointSet, 2)
+        @test timeline.lastestDecrementedTimePoint == 2
+
+        SeaPearl.scheduleTask(timeline, task3)
+        @test timeline.timePointsCapacity[1] == 0
+        @test timeline.timePointsCapacity[2] == 5
+        @test SeaPearl.findRepresentative!(timeline.disjointSet, 1) == SeaPearl.findRepresentative!(timeline.disjointSet, 2)
+        @test timeline.lastestDecrementedTimePoint == 2
+    end
+
+    @testset "earliestCompletionTime(timeline::Timeline)" begin
+        trailer = SeaPearl.Trailer()
+        startingTime1 = SeaPearl.IntVar(1, 3,"t1", trailer)
+        startingTime2 = SeaPearl.IntVar(2, 4,"t1", trailer)
+        startingTime3 = SeaPearl.IntVar(2, 5,"t1", trailer)
+        task1 = SeaPearl.Task(startingTime1, 1, 1)
+        task2 = SeaPearl.Task(startingTime2, 2, 2)
+        task3 = SeaPearl.Task(startingTime3, 1, 3)
+        tasks = [task1, task2, task3]
+        timeline = SeaPearl.Timeline(tasks)
+
+        SeaPearl.scheduleTask(timeline, task1)
+        @test SeaPearl.earliestCompletionTime(timeline) == 2
+
+        SeaPearl.scheduleTask(timeline, task2)
+        @test SeaPearl.earliestCompletionTime(timeline) == 4
+
+        SeaPearl.scheduleTask(timeline, task3)
+        @test SeaPearl.earliestCompletionTime(timeline) == 5
+    end
+
+    @testset "Timeline(tasks::Task) example from paper" begin
+        trailer = SeaPearl.Trailer()
+        startingTime1 = SeaPearl.IntVar(4, 10,"t1", trailer)
+        startingTime2 = SeaPearl.IntVar(1, 4,"t1", trailer)
+        startingTime3 = SeaPearl.IntVar(5, 6,"t1", trailer)
+        task1 = SeaPearl.Task(startingTime1, 5, 1)
+        task2 = SeaPearl.Task(startingTime2, 6, 2)
+        task3 = SeaPearl.Task(startingTime3, 2, 3)
+        tasks = [task1, task2, task3]
+        timeline = SeaPearl.Timeline(tasks)
+
+        @test length(timeline.timePoints) == 4
+        @test timeline.timePoints[1] == 1
+        @test timeline.timePoints[2] == 4
+        @test timeline.timePoints[3] == 5
+        @test timeline.timePoints[4] == 28
+
+
+        @test length(timeline.timePointsCapacity) == 3
+        @test timeline.timePointsCapacity[1] == 3
+        @test timeline.timePointsCapacity[2] == 1
+        @test timeline.timePointsCapacity[3] == 23
+
+        @test length(timeline.mapTaskIndexTimePoint) == 3
+        @test timeline.mapTaskIndexTimePoint[1] == 2
+        @test timeline.mapTaskIndexTimePoint[2] == 1
+        @test timeline.mapTaskIndexTimePoint[3] == 3
+
+        SeaPearl.scheduleTask(timeline, task1)
+        @test timeline.timePointsCapacity[1] == 3
+        @test timeline.timePointsCapacity[2] == 0
+        @test timeline.timePointsCapacity[3] == 19
+        @test SeaPearl.findRepresentative!(timeline.disjointSet, 2) == SeaPearl.findRepresentative!(timeline.disjointSet, 3)
+        @test timeline.lastestDecrementedTimePoint == 3
+        @test SeaPearl.earliestCompletionTime(timeline) == 9
+
+        SeaPearl.scheduleTask(timeline, task2)
+        @test timeline.timePointsCapacity[1] == 0
+        @test timeline.timePointsCapacity[2] == 0
+        @test timeline.timePointsCapacity[3] == 16
+        @test SeaPearl.findRepresentative!(timeline.disjointSet, 1) == SeaPearl.findRepresentative!(timeline.disjointSet, 3)
+        @test SeaPearl.findRepresentative!(timeline.disjointSet, 2) == SeaPearl.findRepresentative!(timeline.disjointSet, 3)
+        @test timeline.lastestDecrementedTimePoint == 3
+        @test SeaPearl.earliestCompletionTime(timeline) == 12
+
+        SeaPearl.scheduleTask(timeline, task3)
+        @test timeline.timePointsCapacity[1] == 0
+        @test timeline.timePointsCapacity[2] == 0
+        @test timeline.timePointsCapacity[3] == 14
+        @test SeaPearl.findRepresentative!(timeline.disjointSet, 1) == SeaPearl.findRepresentative!(timeline.disjointSet, 3)
+        @test SeaPearl.findRepresentative!(timeline.disjointSet, 2) == SeaPearl.findRepresentative!(timeline.disjointSet, 3)
+        @test timeline.lastestDecrementedTimePoint == 3
+        @test SeaPearl.earliestCompletionTime(timeline) == 14
+
+    end
+end
