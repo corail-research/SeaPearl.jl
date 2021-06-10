1217c6a7cc021e8e754e5b4b74d94e3a6d3fa769
PierreTsr <pierre.borie.tessier@gmail.com>
2021-06-10 21:26:34 +0200

    fix: incorrect default initialization

--- src/CP/valueselection/learning/learning.jl
+++ src/CP/valueselection/learning/learning.jl
@@ -3,4 +3,4 @@ include("learnedheuristic.jl")
 include("rewards/rewards.jl")
 include("utils.jl")
 
-LearnedHeuristic(agent::RL.Agent) = LearnedHeuristic{DefaultStateRepresentation, DefaultReward, FixedOutput}(agent)
+LearnedHeuristic(agent::RL.Agent) = LearnedHeuristic{DefaultStateRepresentation{DefaultFeaturization, DefaultTrajectoryState}, DefaultReward, FixedOutput}(agent)
