4971209efb5b51b758709e210a145fbe99b146e6
Pierre Tessier <55246205+PierreTsr@users.noreply.github.com>
2021-06-23 11:24:15 -0400

    fix CPModification update with View Variables (#160)
    As observed almost a year ago in #55 , CPModification isn't updated correctly with IntVarView and BoolVarView.
    I propose a fix by providing a new definition of addToPrunedDomains for VariableViews. After creating/updating the entry corresponding to the view, it recursively call the function for the parent variable until a root variable is found.

--- src/CP/CP.jl
+++ src/CP/CP.jl
@@ -4,15 +4,5 @@ abstract type Constraint end
 abstract type OnePropagationConstraint <: Constraint end
 
 include("variables/variables.jl")
-include("core/model.jl")
+include("core/core.jl")
 include("constraints/constraints.jl")
-
-include("core/fixPoint.jl")
-include("variableselection/variableselection.jl")
-
-include("core/search/strategies.jl")
-
-include("valueselection/valueselection.jl")
-
-include("core/search/search.jl")
-include("core/solver.jl")
---
+++ src/CP/core/core.jl
@@ -0,0 +1,13 @@
+include("model.jl")
+include("cpmodification.jl")
+include("fixPoint.jl")
+
+include("search/strategies.jl")
+
+include("../variableselection/variableselection.jl")
+include("../valueselection/valueselection.jl")
+
+include("search/search.jl")
+
+include("solver.jl")
+
