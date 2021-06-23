b7432ee37ad3396f5776f1baaf18988fb8ebdea3
Pierre Tessier <55246205+PierreTsr@users.noreply.github.com>
2021-06-23 11:28:51 -0400

    Fix/dependencies (#159)
    Some new features recently added to SeaPearl, brought a lot of new dependencies. These dependencies bring more complexity to the compatibility tree of SeaPearl, despite the fact that they aren't critical to the module. In order to avoid the dependencies of SeaPearl to become too messy, I suggest to create a new repo SeaPearlExtras with all the plotting & logging related functions. Thus a user could choose wether or not to load all these additional packages and it would be easier to maintain.

--- src/RL/representation/default/defaultstaterepresentation.jl
+++ src/RL/representation/default/defaultstaterepresentation.jl
@@ -49,19 +49,6 @@ function update_representation!(sr::DefaultStateRepresentation, model::CPModel, 
     return sr
 end
 
-function print_tripartite(sr::DefaultStateRepresentation)
-    cpmodel = sr.cplayergraph
-    n = cpmodel.totalLength
-    nodefillc = []
-    for id in 1:n
-        v = cpmodel.idToNode[id]
-        if isa(v, VariableVertex) push!(nodefillc,"red")
-        elseif isa(v, ValueVertex) push!(nodefillc,"blue")
-        else  push!(nodefillc,"black") end
-    end
-    gplot(cpmodel;nodefillc=nodefillc)
-end
-
 struct DefaultFeaturization <: AbstractFeaturization end
 
 """
