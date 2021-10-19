fb253acc015d6519ef2c5302eb5f72c021538852
tomsander1998 <tom.sander@polytechnique.edu>
2021-10-19 15:11:53 +0200

    Variant of CPReward

--- src/CP/valueselection/learning/rewards/CPReward2.jl
+++ src/CP/valueselection/learning/rewards/CPReward2.jl
@@ -3,20 +3,20 @@ function f(x::AbstractIntVar,alpha::Float64)
 end
 
 """
-    struct CPReward <: AbstractReward end
-This is the default reward, that will be used if no custom reward is specified when constructing the `LearnedHeuristic`.
+    struct CPReward2 <: AbstractReward end
+CPReward2 is a variant of CPReward with better theoretical properties concerning the variations of the function..
 """
-mutable struct CPReward <: AbstractReward
+mutable struct CPReward2 <: AbstractReward
     value::Float32
 end
 
-CPReward(model::CPModel) = CPReward(0)
+CPReward2(model::CPModel) = CPReward(0)
 
 """
     set_reward!(::StepPhase, lh::LearnedHeuristic{DefaultReward, A}, model::CPModel, symbol::Union{Nothing, Symbol})
 Change the "current_reward" attribute of the LearnedHeuristic at the StepPhase.
 """
-function set_reward!(::Type{StepPhase}, lh::LearnedHeuristic{SR, CPReward, A}, model::CPModel, symbol::Union{Nothing, Symbol}) where {
+function set_reward!(::Type{StepPhase}, lh::LearnedHeuristic{SR, CPReward2, A}, model::CPModel, symbol::Union{Nothing, Symbol}) where {
     SR <: AbstractStateRepresentation,
     A <: ActionOutput
 }
@@ -24,11 +24,11 @@ function set_reward!(::Type{StepPhase}, lh::LearnedHeuristic{SR, CPReward, A}, m
 end
 
 """
-    set_reward!(::DecisionPhase, lh::LearnedHeuristic{DefaultReward, O}, model::CPModel)
+    set_reward!(::Type{DecisionPhase}, lh::LearnedHeuristic{SR, CPReward2, A}, model::CPModel)
 Change the current reward at the DecisionPhase. This is called right before making the next decision, so you know you have the very last state before the new decision
 and every computation like fixPoints and backtracking has been done.
 """
-function set_reward!(::Type{DecisionPhase}, lh::LearnedHeuristic{SR, CPReward, A}, model::CPModel) where {
+function set_reward!(::Type{DecisionPhase}, lh::LearnedHeuristic{SR, CPReward2, A}, model::CPModel) where {
     SR <: AbstractStateRepresentation,
     A <: ActionOutput
 }
@@ -37,11 +37,11 @@ end
 
 
 """
-    set_reward!(::EndingPhase, lh::LearnedHeuristic{DefaultReward, A}, model::CPModel, symbol::Union{Nothing, Symbol})
+    set_reward!(::Type{EndingPhase}, lh::LearnedHeuristic{SR, CPReward2, A}, model::CPModel, symbol::Union{Nothing, Symbol})
 Increment the current reward at the EndingPhase. Called when the search is finished by an optimality proof or by a limit in term of nodes or
 in terms of number of solution. This is useful to add some general results to the reward like the number of ndoes visited during the episode for instance.
 """
-function set_reward!(::Type{EndingPhase}, lh::LearnedHeuristic{SR, CPReward, A}, model::CPModel, symbol::Union{Nothing, Symbol}) where {
+function set_reward!(::Type{EndingPhase}, lh::LearnedHeuristic{SR, CPReward2, A}, model::CPModel, symbol::Union{Nothing, Symbol}) where {
     SR <: AbstractStateRepresentation,
     A <: ActionOutput
 }
@@ -53,6 +53,7 @@ function set_reward!(::Type{EndingPhase}, lh::LearnedHeuristic{SR, CPReward, A},
             lh.reward.value += f(model.objective, alpha)
         end
     else
-        lh.reward.value -= length(branchable_variables(model))/nb_boundvariables(model) - 1
+        q = length(branchable_variables(model))/nb_boundvariables(model)
+        lh.reward.value += 0.5*(0.5/q^2 - q)
     end
 end
