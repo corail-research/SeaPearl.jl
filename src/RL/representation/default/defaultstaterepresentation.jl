2769cd04553e64f23c0d6c559c608e342f69704e
Tristan François <tpv.francois@gmail.com>
2022-04-14 11:24:39 -0400

    Fix featurize with FeaturizationHelper

--- src/RL/representation/default/defaultstaterepresentation.jl
+++ src/RL/representation/default/defaultstaterepresentation.jl
@@ -8,33 +8,34 @@ This is the default representation used by SeaPearl unless the user define his o
 It consists in a tripartite graph representation of the CP Model, with features associated with each node
 and an index specifying the variable that should be branched on.
 """
-mutable struct DefaultStateRepresentation{F, TS} <: FeaturizedStateRepresentation{F, TS}
+mutable struct DefaultStateRepresentation{F,TS} <: FeaturizedStateRepresentation{F,TS}
     cplayergraph::CPLayerGraph
-    nodeFeatures::Union{Nothing, AbstractMatrix{Float32}}
-    globalFeatures::Union{Nothing, AbstractVector{Float32}}
-    variableIdx::Union{Nothing, Int64}
-    allValuesIdx::Union{Nothing, Vector{Int64}}
+    nodeFeatures::Union{Nothing,AbstractMatrix{Float32}}
+    globalFeatures::Union{Nothing,AbstractVector{Float32}}
+    variableIdx::Union{Nothing,Int64}
+    allValuesIdx::Union{Nothing,Vector{Int64}}
+    chosenFeatures::Union{Nothing, Dict{String, Bool}}
 end
 
-function DefaultStateRepresentation{F, TS}(model::CPModel; action_space=nothing) where {F, TS}
+function DefaultStateRepresentation{F,TS}(model::CPModel; action_space=nothing) where {F,TS}
     g = CPLayerGraph(model)
     allValuesIdx = nothing
     if !isnothing(action_space)
         allValuesIdx = indexFromCpVertex.([g], ValueVertex.(action_space))
     end
-    sr = DefaultStateRepresentation{F, TS}(g, nothing, nothing, nothing, allValuesIdx)
+    sr = DefaultStateRepresentation{F,TS}(g, nothing, nothing, nothing, allValuesIdx, nothing)
     sr.nodeFeatures = featurize(sr)
     sr.globalFeatures = global_featurize(sr)
     return sr
 end
 
-function DefaultTrajectoryState(sr::DefaultStateRepresentation{F, DefaultTrajectoryState}) where F
+function DefaultTrajectoryState(sr::DefaultStateRepresentation{F,DefaultTrajectoryState}) where {F}
     if isnothing(sr.variableIdx)
         throw(ErrorException("Unable to build a DefaultTrajectoryState, when the branching variable is nothing."))
     end
     adj = Matrix(adjacency_matrix(sr.cplayergraph))
-    fg = isnothing(sr.globalFeatures) ? 
-        FeaturedGraph(adj; nf=sr.nodeFeatures) : FeaturedGraph(adj; nf=sr.nodeFeatures, gf=sr.globalFeatures)
+    fg = isnothing(sr.globalFeatures) ?
+         FeaturedGraph(adj; nf=sr.nodeFeatures) : FeaturedGraph(adj; nf=sr.nodeFeatures, gf=sr.globalFeatures)
     return DefaultTrajectoryState(fg, sr.variableIdx, sr.allValuesIdx)
 end
 
@@ -60,7 +61,7 @@ Create features for every node of the graph. Supposed to be overwritten.
 
 Default behavior consists in a 3D One-hot vector that encodes whether the node represents a Constraint, a Variable or a Value.
 """
-function featurize(sr::FeaturizedStateRepresentation{DefaultFeaturization, TS}) where TS
+function featurize(sr::FeaturizedStateRepresentation{DefaultFeaturization,TS}) where {TS}
     g = sr.cplayergraph
     features = zeros(Float32, 3, nv(g))
     for i in 1:nv(g)
@@ -81,14 +82,13 @@ end
 """
 Featurization helper: initializes the graph with the features specified as arguments.
 """
-
-function featurize(sr::FeaturizedStateRepresentation{FeaturizationHelper,TS};values_onehot=false, constraint_activity=true, variable_initial_domain_size=true, nb_involved_contraint_propagation=true) where TS
+function featurize(sr::DefaultStateRepresentation{FeaturizationHelper,TS}; values_onehot::Bool=false, constraint_activity::Bool=true, variable_initial_domain_size::Bool=true, nb_involved_contraint_propagation::Bool=true) where {TS}
     g = sr.cplayergraph
-    global nb_features = 3
+    nb_features = 3
     if values_onehot
         nb_features += g.numberOfValues
     end
-    nb_features += int(constraint_activity) + int(variable_initial_domain_size) + int(nb_involved_contraint_propagation)
+    nb_features += constraint_activity + variable_initial_domain_size + nb_involved_contraint_propagation
 
     features = zeros(Float32, nb_features, nv(g))
     for i in 1:nv(g)
@@ -97,7 +97,7 @@ function featurize(sr::FeaturizedStateRepresentation{FeaturizationHelper,TS};val
         if isa(cp_vertex, ConstraintVertex)
             features[1, i] = 1.0f0
             if constraint_activity
-                features[counter, i] = cp_vertex.constraint.active
+                features[counter, i] = cp_vertex.constraint.active.value
                 counter += 1
             end
             if nb_involved_contraint_propagation
@@ -115,14 +115,19 @@ function featurize(sr::FeaturizedStateRepresentation{FeaturizationHelper,TS};val
         if isa(cp_vertex, ValueVertex)
             features[3, i] = 1.0f0
             if values_onehot
-                cp_vertex_idx = find(x->x==cp_vertex.value,sr.allValuesIdx)
-                features[counter+cp_vertex_idx,i] = 1
+                cp_vertex_idx = find(x -> x == cp_vertex.value, sr.allValuesIdx)
+                features[counter+cp_vertex_idx, i] = 1
             else
                 features[counter, i] = cp_vertex.value
             end
         end
     end
-    sr.features_chosen = []
+    sr.chosenFeatures = Dict{String,Bool}(
+        "values_onehot" => values_onehot,
+        "constraint_activity" => constraint_activity,
+        "variable_initial_domain_size" => variable_initial_domain_size,
+        "nb_involved_contraint_propagation" => nb_involved_contraint_propagation
+    )
     return features
 end
 """
@@ -139,8 +144,10 @@ end
 
 Returns the length of the feature vector, for the `DefaultFeaturization`.
 """
-feature_length(::Type{<:FeaturizedStateRepresentation{DefaultFeaturization, TS}}) where TS = 3
+feature_length(::Type{<:FeaturizedStateRepresentation{DefaultFeaturization,TS}}) where {TS} = 3
 
-feature_length(::Type{<:FeaturizedStateRepresentation{FeaturizationHelper, TS}}) where TS = nb_features
+DefaultStateRepresentation(m::CPModel) = DefaultStateRepresentation{DefaultFeaturization,DefaultTrajectoryState}(m::CPModel)
 
-DefaultStateRepresentation(m::CPModel) = DefaultStateRepresentation{DefaultFeaturization, DefaultTrajectoryState}(m::CPModel)
+# function update_features!(sr::Type{<:FeaturizedStateRepresentation{FeaturizationHelper,TS}}, ::CPModel) where {TS}
+#     println(sr.features_chosen)
+# end
