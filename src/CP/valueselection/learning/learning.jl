2c158c3dc877499ad151bf0dd2701d06405d90db
Ziad <ziadelassal@gmail.com>
2022-05-04 11:23:37 -0400

    Add the new definition of SupervisedLearnedHeuristic to learning.jl

--- src/CP/valueselection/learning/learning.jl
+++ src/CP/valueselection/learning/learning.jl
@@ -5,4 +5,22 @@ include("utils.jl")
 include("supervisedlearnedheuristic.jl")
 
 SimpleLearnedHeuristic(agent::RL.Agent) = SimpleLearnedHeuristic{DefaultStateRepresentation{DefaultFeaturization, DefaultTrajectoryState}, DefaultReward, FixedOutput}(agent)
-SupervisedLearnedHeuristic(agent::RL.Agent) = SupervisedLearnedHeuristic{DefaultStateRepresentation{DefaultFeaturization, DefaultTrajectoryState}, DefaultReward, FixedOutput}(agent)
+
+SupervisedLearnedHeuristic(agent::RL.Agent;
+    helpVariableHeuristic::AbstractVariableSelection=MinDomainVariableSelection(),
+    helpValueHeuristic::ValueSelection=BasicHeuristic(),
+    eta_init::Float64=0.5,
+    eta_stable::Float64=0.5,
+    warmup_steps::Int64=0,
+    decay_steps::Int64=0,
+    rng::Union{Nothing, AbstractRNG}=MersenneTwister()
+) = SupervisedLearnedHeuristic{DefaultStateRepresentation{DefaultFeaturization, DefaultTrajectoryState}, DefaultReward, FixedOutput}(
+        agent;
+        helpVariableHeuristic,
+        helpValueHeuristic,
+        eta_init,
+        eta_stable,
+        warmup_steps,
+        decay_steps,
+        rng
+    )
