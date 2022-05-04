a1cf6b47b4ca0d72bd4099bd0cae9c1067ec4871
Tristan François <33726628+gostreap@users.noreply.github.com>
2022-05-04 11:09:21 -0400

    Add RandomHeuristic for value (#219)
    * Add RandomHeuristic
    
    * fix test name

---
+++ src/CP/valueselection/classic/random.jl
@@ -0,0 +1,23 @@
+"""
+    select_random_value(x::SeaPearl.IntVar, rng::Union{Nothing,AbstractRNG})
+
+Selects a random value of the domain of `x`.
+
+A random generator `rng` can be specified, notably to ensure the reproducibility of results.
+"""
+function select_random_value(x::SeaPearl.IntVar, rng::Union{Nothing,AbstractRNG})
+    if isnothing(rng)
+        return rand(x.domain.values[1:x.domain.size.value]) + x.domain.offset
+    else
+        return rand(rng, x.domain.values[1:x.domain.size.value]) + x.domain.offset
+    end
+end
+
+"""
+    RandomHeuristic(rng::Union{Nothing,AbstractRNG}=nothing)
+
+Create a `BasicHeuristic` that selects a random value of the domain.
+
+A random generator `rng` can be specified, notably to ensure the reproducibility of results.
+"""
+RandomHeuristic(rng::Union{Nothing,AbstractRNG}=nothing) = BasicHeuristic((x; cpmodel = nothing) -> select_random_value(x, rng), nothing)
