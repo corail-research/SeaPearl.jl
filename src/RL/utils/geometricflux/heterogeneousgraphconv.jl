e4b09c01abd2c23550e1a0888f5fadd39484a15e
louis-gautier <louisgautier99@gmail.com>
2022-05-09 16:35:48 -0400

    Consistent order of types of nodes + naming adjustments

--- src/RL/utils/geometricflux/heterogeneousgraphconv.jl
+++ src/RL/utils/geometricflux/heterogeneousgraphconv.jl
@@ -23,31 +23,29 @@ end
 Flux.@functor HeterogeneousGraphConv
 
 function (g::HeterogeneousGraphConv)(fgs::BatchedHeterogeneousFeaturedGraph{Float32}, original_fgs::BatchedHeterogeneousFeaturedGraph{Float32}) 
-    A11, A12, A13 = fgs.A11, fgs.A12, fgs.A13
-    H1, H2, H3 = fgs.nf1, fgs.nf2, fgs.nf3
-    X1, X2, X3 = original_fgs.nf1, original_fgs.nf2, original_fgs.nf3
+    contovar, valtovar = fgs.contovar, fgs.valtovar
+    H1, H2, H3 = fgs.varnf, fgs.connf, fgs.valnf
+    X1, X2, X3 = original_fgs.varnf, original_fgs.connf, original_fgs.valnf
     return BatchedHeterogeneousFeaturedGraph{Float32}(
-        A11,
-        A12,
-        A13,
-        g.σ.(g.weights1 ⊠ vcat(X1, H1, A11 ⊠ H1, transpose(A12) ⊠ H2, transpose(A13) ⊠ H3) .+ g.bias1),
-        g.σ.(g.weights2 ⊠ vcat(X2, H2, A12 ⊠ H1) .+ g.bias2),
-        g.σ.(g.weights3 ⊠ vcat(X3, H3, A13 ⊠ H1) .+ g.bias3),
+        contovar,
+        valtovar,
+        g.σ.(g.weights1 ⊠ vcat(X1, H1, contovar ⊠ H2, valtovar ⊠ H3) .+ g.bias1),
+        g.σ.(g.weights2 ⊠ vcat(X2, H2, transpose(contovar) ⊠ H1) .+ g.bias2),
+        g.σ.(g.weights3 ⊠ vcat(X3, H3, transpose(valtovar) ⊠ H1) .+ g.bias3),
         fg.gf
     )
 end
 
 function (g::HeterogeneousGraphConv)(fg::HeterogeneousFeaturedGraph, original_fg::HeterogeneousFeaturedGraph)
-    A11, A12, A13 = fg.A11, fg.A12, fg.A13
-    H1, H2, H3 = fg.nf1, fg.nf2, fg.nf3
-    X1, X2, X3 = original_fg.nf1, original_fg.nf2, original_fg.nf3
+    contovar, valtovar = fg.contovar, fg.valtovar
+    H1, H2, H3 = fg.varnf, fg.connf, fg.valnf
+    X1, X2, X3 = original_fg.varnf, original_fg.connf, original_fg.valnf
     return HeterogeneousFeaturedGraph(
-        A11,
-        A12,
-        A13,
-        g.σ.(g.weights1 * vcat(X1, H1, A11 * H1, transpose(A12) * H2, transpose(A13) * H3) .+ g.bias1),
-        g.σ.(g.weights2 * vcat(X2, H2, A12 * H1) .+ g.bias2),
-        g.σ.(g.weights3 * vcat(X3, H3, A13 * H1) .+ g.bias3),
+        contovar,
+        valtovar,
+        g.σ.(g.weights1 * vcat(X1, H1, contovar * H2, valtovar * H3) .+ g.bias1),
+        g.σ.(g.weights2 * vcat(X2, H2, transpose(contovar) * H1) .+ g.bias2),
+        g.σ.(g.weights3 * vcat(X3, H3, transpose(valtovar) * H1) .+ g.bias3),
         fg.gf
     )
 end
@@ -55,13 +53,13 @@ end
 #=
 # Alternative formulation
 function (g::HeterogeneousGraphConv)(fg::HeterogeneousFeaturedGraph, original_fg::HeterogeneousFeaturedGraph)
-    A11, A12, A13 = fg.A11, fg.A12, fg.A13
+    A11, contovar, valtovar = fg.A11, fg.contovar, fg.valtovar
     ind1, ind2, ind3 = fg.ind1, fg.ind2, fg.ind3
     H = fg.vectors
     X = original_fg.vectors
     return HeterogeneousFeaturedGraph(
         fg.graph,
-        g.σ.(ind1 .* (g.weights1 * vcat(X, H, A11 * H, A12 * H, A13 * H) .+ g.bias1) .+ ind2 .* (g.weights2 * vcat(X, H, transpose(A12)*fg.vectors, A23 * H) .+ g.bias2) .+ ind3 .* (g.weights3 * vcat(X, H, transpose(A13) * H, transpose(A23) * H) .+ g.bias3)),
+        g.σ.(ind1 .* (g.weights1 * vcat(X, H, A11 * H, contovar * H, valtovar * H) .+ g.bias1) .+ ind2 .* (g.weights2 * vcat(X, H, transpose(contovar)*fg.vectors, A23 * H) .+ g.bias2) .+ ind3 .* (g.weights3 * vcat(X, H, transpose(valtovar) * H, transpose(A23) * H) .+ g.bias3)),
         fg.ef,
         fg.gf,
         fg.directed
