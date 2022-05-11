61ba319514cd2a7654dc5d5444b2ecec432d6272
Tristan François <tpv.francois@gmail.com>
2022-05-11 14:48:44 -0400

    Add test for HeterogeneousSR with chosen_features

--- test/RL/representation/default/heterogeneousstaterepresentation.jl
+++ test/RL/representation/default/heterogeneousstaterepresentation.jl
@@ -63,4 +63,115 @@
         @test hsr.variableIdx == 2
     end
 
+
+    @testset "HeterogeneousSR from CPModel with chosen_features 1" begin
+        trailer = SeaPearl.Trailer()
+        model = SeaPearl.CPModel(trailer)
+
+        x = SeaPearl.IntVar(3, 3, "x", trailer)
+        y = SeaPearl.IntVar(1, 3, "y", trailer)
+        SeaPearl.addVariable!(model, x)
+        SeaPearl.addVariable!(model, y)
+        SeaPearl.addConstraint!(model, SeaPearl.Equal(x, y, trailer))
+        SeaPearl.addConstraint!(model, SeaPearl.NotEqual(x, y, trailer))
+
+        chosen_features = Dict(
+            "constraint_type" => true,
+            "variable_initial_domain_size" => true,
+            "values_raw" => true,
+        )
+
+        hsr = SeaPearl.HeterogeneousStateRepresentation{SeaPearl.DefaultFeaturization, SeaPearl.HeterogeneousTrajectoryState}(model; chosen_features=chosen_features)
+        SeaPearl.update_representation!(hsr, model, x)
+
+        @test Matrix(LightGraphs.LinAlg.adjacency_matrix(hsr.cplayergraph)) == [
+            0 0 1 1 0 0 0
+            0 0 1 1 0 0 0
+            1 1 0 0 0 1 0
+            1 1 0 0 1 1 1
+            0 0 0 1 0 0 0
+            0 0 1 1 0 0 0
+            0 0 0 1 0 0 0]
+
+        @test hsr.variableNodeFeatures == [1 3]
+        @test hsr.constraintNodeFeatures == [1 0; 0 1]
+        @test hsr.valueNodeFeatures == [2 3 1]
+        @test hsr.variableIdx == 1
+
+        SeaPearl.assign!(y, 2)
+        SeaPearl.update_representation!(hsr, model, y)
+
+        @test Matrix(LightGraphs.LinAlg.adjacency_matrix(hsr.cplayergraph)) == [
+            0 0 1 1 0 0 0
+            0 0 1 1 0 0 0
+            1 1 0 0 0 1 0
+            1 1 0 0 1 0 0
+            0 0 0 1 0 0 0
+            0 0 1 0 0 0 0
+            0 0 0 0 0 0 0]
+
+        @test hsr.variableNodeFeatures == [1 3]
+        @test hsr.constraintNodeFeatures == [1 0; 0 1]
+        @test hsr.valueNodeFeatures == [2 3 1]
+        @test hsr.variableIdx == 2
+    end
+
+    @testset "HeterogeneousSR from CPModel with chosen_features 2" begin
+        trailer = SeaPearl.Trailer()
+        model = SeaPearl.CPModel(trailer)
+
+        x = SeaPearl.IntVar(3, 3, "x", trailer)
+        y = SeaPearl.IntVar(1, 3, "y", trailer)
+        SeaPearl.addVariable!(model, x)
+        SeaPearl.addVariable!(model, y)
+        SeaPearl.addConstraint!(model, SeaPearl.Equal(x, y, trailer))
+        SeaPearl.addConstraint!(model, SeaPearl.NotEqual(x, y, trailer))
+
+        chosen_features = Dict(
+            "constraint_type" => true,
+            "nb_not_bounded_variable" => true,
+            "constraint_activity" => true,
+            "variable_initial_domain_size" => true,
+            "variable_domain_size" => true,
+            "variable_is_bound" => true,
+            "values_onehot" => true,
+        )
+
+        action_space = SeaPearl.branchable_values(model)
+        hsr = SeaPearl.HeterogeneousStateRepresentation{SeaPearl.DefaultFeaturization, SeaPearl.HeterogeneousTrajectoryState}(model; action_space=action_space, chosen_features=chosen_features)
+        SeaPearl.update_representation!(hsr, model, x)
+
+        @test Matrix(LightGraphs.LinAlg.adjacency_matrix(hsr.cplayergraph)) == [
+            0 0 1 1 0 0 0
+            0 0 1 1 0 0 0
+            1 1 0 0 0 1 0
+            1 1 0 0 1 1 1
+            0 0 0 1 0 0 0
+            0 0 1 1 0 0 0
+            0 0 0 1 0 0 0]
+
+        @test hsr.variableNodeFeatures == [1 3; 1 3; 1 0]
+        @test hsr.constraintNodeFeatures == [1 1; 1 1; 1 0; 0 1]
+        @test hsr.valueNodeFeatures == [1 0 0; 0 1 0; 0 0 1]
+        @test hsr.variableIdx == 1
+
+        SeaPearl.assign!(y, 2)
+        SeaPearl.update_representation!(hsr, model, y)
+
+        @test Matrix(LightGraphs.LinAlg.adjacency_matrix(hsr.cplayergraph)) == [
+            0 0 1 1 0 0 0
+            0 0 1 1 0 0 0
+            1 1 0 0 0 1 0
+            1 1 0 0 1 0 0
+            0 0 0 1 0 0 0
+            0 0 1 0 0 0 0
+            0 0 0 0 0 0 0]
+
+        @test hsr.variableNodeFeatures == [1 3; 1 1; 1 1]
+        @test hsr.constraintNodeFeatures == [1 1; 0 0; 1 0; 0 1]
+        @test hsr.valueNodeFeatures == [1 0 0; 0 1 0; 0 0 1]
+        @test hsr.variableIdx == 2
+    end
+
+
 end
