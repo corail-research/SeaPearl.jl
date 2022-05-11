edf67aae157e127276b6d555a74893bb96fedb80
Tristan François <tpv.francois@gmail.com>
2022-05-11 15:51:55 -0400

    test HeterogeneousTrajectoryState constructor + heterogeneous on square graph

--- test/RL/representation/default/heterogeneousstaterepresentation.jl
+++ test/RL/representation/default/heterogeneousstaterepresentation.jl
@@ -136,8 +136,8 @@
             "variable_is_bound" => true,
             "values_onehot" => true,
         )
-
         action_space = SeaPearl.branchable_values(model)
+        
         hsr = SeaPearl.HeterogeneousStateRepresentation{SeaPearl.DefaultFeaturization, SeaPearl.HeterogeneousTrajectoryState}(model; action_space=action_space, chosen_features=chosen_features)
         SeaPearl.update_representation!(hsr, model, x)
 
@@ -173,5 +173,114 @@
         @test hsr.variableIdx == 2
     end
 
+
+    @testset "HeterogeneousTrajectoryState constructor" begin
+        trailer = SeaPearl.Trailer()
+        model = SeaPearl.CPModel(trailer)
+    
+        x = SeaPearl.IntVar(2, 3, "x", trailer)
+        y = SeaPearl.IntVar(2, 3, "y", trailer)
+        SeaPearl.addVariable!(model, x)
+        SeaPearl.addVariable!(model, y)
+        SeaPearl.addConstraint!(model, SeaPearl.Equal(x, y, trailer))
+        SeaPearl.addConstraint!(model, SeaPearl.NotEqual(x, y, trailer))
+
+        chosen_features = Dict(
+            "constraint_type" => true,
+            "nb_not_bounded_variable" => true,
+            "constraint_activity" => true,
+            "variable_initial_domain_size" => true,
+            "variable_domain_size" => true,
+            "variable_is_bound" => true,
+            "values_onehot" => true,
+        )
+        action_space = SeaPearl.branchable_values(model)
+
+        hsr = SeaPearl.HeterogeneousStateRepresentation{SeaPearl.DefaultFeaturization, SeaPearl.HeterogeneousTrajectoryState}(model; action_space=action_space, chosen_features=chosen_features)
+        SeaPearl.update_representation!(hsr, model, x) #add x as the branching variable
+        hts = SeaPearl.HeterogeneousTrajectoryState(hsr)  #creates the HeterogeneousTrajectoryState object
+
+        batchedHtsSingle = hts |> cpu   
+        @test batchedHtsSingle.fg.contovar[:,:,1]== hts.fg.contovar
+        @test batchedHtsSingle.fg.valtovar[:,:,1]== hts.fg.valtovar
+        @test batchedHtsSingle.fg.varnf[:,:,1] == hts.fg.varnf
+        @test batchedHtsSingle.fg.connf[:,:,1] == hts.fg.connf
+        @test batchedHtsSingle.fg.valnf[:,:,1] == hts.fg.valnf
+        @test batchedHtsSingle.fg.gf[:,1] == hts.fg.gf
+        @test batchedHtsSingle.variableIdx[1] == hts.variableIdx
+
+        SeaPearl.assign!(x, 2)
+        SeaPearl.fixPoint!(model, SeaPearl.getOnDomainChange(x))
+        SeaPearl.update_representation!(hsr, model, x) #add x as the branching variable
+        hts2 = SeaPearl.HeterogeneousTrajectoryState(hsr)  #creates the HeterogeneousTrajectoryState object
+
+        batchedHts = [hts,hts2] |> cpu   
+        @test size(batchedHts.fg.contovar,3) == 2
+        @test size(batchedHts.fg.valtovar,3) == 2
+        @test size(batchedHts.fg.varnf,3) == 2
+        @test size(batchedHts.fg.connf,3) == 2
+        @test size(batchedHts.fg.valnf,3) == 2
+        @test size(batchedHts.fg.gf,2) == 2
+        @test size(batchedHts.variableIdx,1) == 2
+    end
+
+    @testset "HeterogeneousSR with on the square graph coloring problem" begin
+        trailer = SeaPearl.Trailer()
+        model = SeaPearl.CPModel(trailer)
+
+        # Testing chosen_features on a toy example: the square graph coloring problem
+        x = SeaPearl.IntVar[]
+        for i in 1:4
+            push!(x, SeaPearl.IntVar(1, 4, string(i), model.trailer))
+            SeaPearl.addVariable!(model, last(x))
+        end
+        # Edge constraints
+        SeaPearl.addConstraint!(model, SeaPearl.NotEqual(x[1], x[2], model.trailer))
+        SeaPearl.addConstraint!(model, SeaPearl.NotEqual(x[2], x[3], model.trailer))
+        SeaPearl.addConstraint!(model, SeaPearl.NotEqual(x[3], x[4], model.trailer))
+        SeaPearl.addConstraint!(model, SeaPearl.NotEqual(x[4], x[1], model.trailer))
+        # Objective
+        numberOfColors = SeaPearl.IntVar(1, 4, "numberOfColors", model.trailer)
+        SeaPearl.addVariable!(model, numberOfColors)
+        for var in x
+            SeaPearl.addConstraint!(model, SeaPearl.LessOrEqual(var, numberOfColors, model.trailer))
+        end
+        model.objective = numberOfColors
+        
+        # Choosing features and initializing the state representation
+        chosen_features = Dict(
+            "constraint_type" => true,
+            "nb_not_bounded_variable" => true,
+            "constraint_activity" => true,
+            "variable_initial_domain_size" => true,
+            "variable_domain_size" => true,
+            "variable_is_bound" => true,
+            "values_onehot" => true,
+        )
+        sr = SeaPearl.HeterogeneousStateRepresentation{SeaPearl.DefaultFeaturization, SeaPearl.HeterogeneousTrajectoryState}(model; action_space = 1:4, chosen_features=chosen_features)
+
+        @test SeaPearl.branchable_values(model) == [4, 2, 3, 1]
+
+        # Testing the initialization of the node features
+        @test sr.variableNodeFeatures == [4 4 4 4 4; 4 4 4 4 4; 0 0 0 0 0]
+        @test sr.constraintNodeFeatures == [1 1 1 1 1 1 1 1; 2 2 2 2 2 2 2 2; 1 1 1 1 0 0 0 0; 0 0 0 0 1 1 1 1]
+        @test sr.valueNodeFeatures == [0 0 0 1; 0 1 0 0; 0 0 1 0; 1 0 0 0]
+
+        # assign color 1 to x[1]
+        prunedDomains = SeaPearl.CPModification();
+        SeaPearl.addToPrunedDomains!(prunedDomains, x[1], SeaPearl.assign!(x[1].domain, 1));
+        feasible, pruned = SeaPearl.fixPoint!(model, nothing, prunedDomains)
+        SeaPearl.update_features!(sr, model)
+
+        # Testing the node features after fixPoint!
+        println(sr.variableNodeFeatures)
+        println(sr.constraintNodeFeatures)
+
+        @test sr.variableNodeFeatures == [4 4 4 4 4; 1 3 4 3 3; 1 0 0 0 0]
+        @test sr.constraintNodeFeatures == [0 1 1 0 0 1 1 1; 1 2 2 1 1 2 2 2; 1 1 1 1 0 0 0 0; 0 0 0 0 1 1 1 1]
+        @test sr.valueNodeFeatures == [0 0 0 1; 0 1 0 0; 0 0 1 0; 1 0 0 0]
+
+
+    end
 
 end
