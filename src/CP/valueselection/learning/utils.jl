721889d05dccc6dc84dc683b60b55959012297df
Ziad <ziadelassal@gmail.com>
2022-05-30 15:00:09 -0400

    Rewrite from_id_to_order function

--- src/CP/valueselection/learning/utils.jl
+++ src/CP/valueselection/learning/utils.jl
@@ -185,12 +185,22 @@ end
 """
     from_id_to_order(state::Union{DefaultTrajectoryState, BatchedDefaultTrajectoryState, TsptwTrajectoryState}, value_id::Int64)
 
-Returns the index of the given `value_id` in the `state.possibleValuesIdx`. Used to work in `supervisedLeanedHeuristic` 
-to retrieve an action (which is the index in `state.possibleValuesIdx`) from a value. 
+Returns the index of the given `value_id` (sometimes named `vertex_id`) in either one of `state.possibleValuesIdx` or `state.allValuesIdx`
+according to the argument `which_list`. It can be either 'possibleValuesIdx' or 'allValuesIdx'.
+
+Used to work in `supervisedLeanedHeuristic.jl` to retrieve an action (which is the index in `state.possibleValuesIdx`) from a value, 
+and in `variableoutputcpnn.jl`. 
+
 """
-function from_id_to_order(state::Union{DefaultTrajectoryState, BatchedDefaultTrajectoryState, TsptwTrajectoryState}, value_id::Int64)
+function from_id_to_order(state::Union{DefaultTrajectoryState, BatchedDefaultTrajectoryState, TsptwTrajectoryState}, value_id::Int64; which_list="possibleValuesIdx", idx_in_batch=1)
     @assert !isnothing(state.possibleValuesIdx)
-    return findfirst(x->x == value_id, state.possibleValuesIdx)
+    if which_list == "possibleValuesIdx"
+        return findfirst(x->x == value_id, state.possibleValuesIdx)
+    elseif which_list == "allValuesIdx"
+        return findfirst(x->x == value_id, state.allValuesIdx[:,idx_in_batch])
+    else
+        throw(DomainError(which_list, "which_list should be either 'possibleValuesIdx' or 'allValuesIdx'"))
+    end
 end
 """
     action_to_value(vs::LearnedHeuristic{SR, R, VariableOutput}, action::Int64, state::AbstractArray, model::CPModel)
