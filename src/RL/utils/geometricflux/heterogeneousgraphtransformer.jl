e8ef11deea81891631adc8db2d1a913197800e67
louis-gautier <louisgautier99@gmail.com>
2022-05-30 17:01:46 -0400

    Definition draft for HGT

--- src/RL/utils/geometricflux/heterogeneousgraphtransformer.jl
+++ src/RL/utils/geometricflux/heterogeneousgraphtransformer.jl
@@ -1,9 +1,27 @@
 struct HeterogeneousGraphTransformer
-    
+    in_channels:: Int
+    out_channels:: Int
+    heads:: Int
+    aggr:: String = "sum"
+    k_lin_var::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    k_lin_con::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    k_lin_val::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    q_lin_var::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    q_lin_con::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    q_lin_val::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    m_lin_var::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    m_lin_con::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    m_lin_val::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    a_lin_var::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    a_lin_con::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    a_lin_val::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(in_channels,out_channels,identity),heads)
+    w_att::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(out_channels//heads,out_channels//heads,identity),heads)
+    w_mess::::Vector{Flux.Dense} = Vector{Flux.Dense}(Flux.Dense(out_channels//heads,out_channels//heads,identity),heads)
+    dim::Int = out_channels//heads
 end
 # Constructor
-function HeterogeneousGraphTransformer()
-
+function HeterogeneousGraphTransformer(in_channels::Int, out_channels::Int, heads::Int)
+    @assert out_channels%heads==0
 end
 Flux.@functor HeterogeneousGraphTransformer
 
