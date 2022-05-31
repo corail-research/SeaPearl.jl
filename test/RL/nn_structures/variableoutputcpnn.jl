2462e6a317d476dc09d716e548c92326beb24bd1
Ziad <ziadelassal@gmail.com>
2022-05-31 15:07:31 -0400

    Add test for variableoutputcpnn

--- test/RL/nn_structures/variableoutputcpnn.jl
+++ test/RL/nn_structures/variableoutputcpnn.jl
@@ -1,6 +1,6 @@
 @testset "variableoutputcpnn.jl" begin
     
-    @testset "VariableOutputCPNN" begin
+    @testset "VariableOutputCPNN on DefaultStateRepresentation" begin
         modelNN = SeaPearl.VariableOutputCPNN(
             graphChain = Flux.Chain(
                 SeaPearl.GraphConv(3 => 3),
@@ -15,16 +15,34 @@
             )
         )
 
-        graphs = Matrix.(adjacency_matrix.([random_regular_graph(10, 4) for i = 1:4]))
+        graphs = Matrix.(adjacency_matrix.([random_regular_graph(10, 4; seed=42) for i = 1:4]))
         nodeFeatures = [rand(3, 10) for i = 1:4]
+
         featuredGraphs = [SeaPearl.FeaturedGraph(g; nf=nf) for (g, nf) in zip(graphs, nodeFeatures)]
-
-        trajectoryVector = SeaPearl.TsptwTrajectoryState.(featuredGraphs, rand(1:10, 4), [rand(1:10, rand(1:5)) for i = 1:4])
-        actionSize = map(ts -> length(ts.possibleValuesIdx), trajectoryVector)
+        variableIndices = [1,3,4,7]
+        allValuesIndices = [Vector(1:5) for i = 1:4]
+        possibleValuesIndices = [[1,2,4],[4,2,3,1,5],[2],[5,3]]
+        trajectoryVector = SeaPearl.DefaultTrajectoryState.(featuredGraphs, variableIndices, allValuesIndices, possibleValuesIndices)
+        
         nnInput = trajectoryVector |> cpu
+        
+        output = modelNN(nnInput)
 
-        output = modelNN.(nnInput)
+        @test size(output) == (length(allValuesIndices[1]), length(allValuesIndices))
+        
+        inf_indices = [
+            CartesianIndex(3,1), CartesianIndex(5,1), 
+            CartesianIndex(1,3), CartesianIndex(3,3), CartesianIndex(4,3), CartesianIndex(5,3),
+            CartesianIndex(1,4), CartesianIndex(2,4), CartesianIndex(4,4)
+            ]
+        @test all(output[inf_indices] .== -Inf)
 
-        @test all(length.(output) .== actionSize)
+        float_indices = [
+            CartesianIndex(1,1), CartesianIndex(2,1), CartesianIndex(4,1),
+            CartesianIndex(1,2), CartesianIndex(2,2), CartesianIndex(3,2), CartesianIndex(4,2), CartesianIndex(5,2),
+            CartesianIndex(2,3),
+            CartesianIndex(3,4), CartesianIndex(5,4)
+            ]
+        @test all(output[float_indices] .!= -Inf)
     end
 end
