baff1802d2dd75b063cc06ab66eabf27f1992748
Ziad <ziadelassal@gmail.com>
2022-06-07 12:43:23 -0400

    Clean variableOutputCPNN and heterogeneousVariableOutputCPNN

--- src/RL/nn_structures/variableoutputcpnn.jl
+++ src/RL/nn_structures/variableoutputcpnn.jl
@@ -17,61 +17,42 @@ end
 # Enable the `|> gpu` syntax from Flux
 Flux.@functor VariableOutputCPNN
 
-wears_mask(s::VariableOutputCPNN) = false
+wears_mask(s::VariableOutputCPNN) = true
 
 function (nn::VariableOutputCPNN)(state::BatchedDefaultTrajectoryState)
-    
     variableIdx = state.variableIdx #Vector of size B
-    #println("variableIdx: ", variableIdx)
     batchSize = length(variableIdx)
-
-    #println("batchSize: ", batchSize)
-    #println("allValuesIdx: ", state.allValuesIdx)
-    #display(state.allValuesIdx)
-    
     possibleValuesIdx = [deepcopy(indexes) for indexes in state.possibleValuesIdx]
     
     @assert batchSize == length(possibleValuesIdx)
-    #println("possibleValuesIdx: ", possibleValuesIdx) 
-    #display(possibleValuesIdx)
     actionSpaceSizes = [length(possibleValuesIdxPerVar) for possibleValuesIdxPerVar in possibleValuesIdx]
-    #println("actionSpaceSizes: ", actionSpaceSizes)
-    maxActionSpaceSize = Base.maximum(actionSpaceSizes) #the largest action space size in the batch
-    #println("maxActionSpaceSize: ", maxActionSpaceSize)
-
+    maxActionSpaceSize = length(state.allValuesIdx) #Base.maximum(actionSpaceSizes)
+    #the largest action space size in the batch
+    
     # chain working on the graph(s)
     nodeFeatures = nn.graphChain(state.fg).nf #matrix of size FxNxB (features after graphChain x # of nodes x batch_size)
-    #println("nodeFeatures: ", size(nodeFeatures))
-
+    
     # extract the feature(s) of the variable(s) we're working on
     indices = nothing
     Zygote.ignore() do
         indices = CartesianIndex.(zip(variableIdx, 1:batchSize))
     end
-    #println("indices: ")
-    #display(indices)
     variableFeatures = nothing
     numPadded = nothing
     Zygote.ignore() do
         variableFeatures = reshape(nodeFeatures[:, indices], (:,1,batchSize)) # Fx1xB
-        #println("variableFeatures: ", size(variableFeatures)) 
         # extract the feature(s) of the variable(s) we're working on
         numPadded = [maxActionSpaceSize - actionSpaceSizes[i] for i in 1:batchSize] #number of padding zeros needed fo each element of the batch
-        #println("numPadded: ", numPadded)
     end
     
     valueIndices = nothing
     Zygote.ignore() do 
         paddedPossibleValuesIdx = [append!(possibleValuesIdx[i], repeat([possibleValuesIdx[i][1]], numPadded[i])) for i in 1:batchSize]
-        #println("paddedPossibleValuesIdx: ", paddedPossibleValuesIdx)
         paddedPossibleValuesIdx = mapreduce(identity, hcat, paddedPossibleValuesIdx) #convert from Vector to Matrix
         #create a CartesianIndex matrix of size (maxActionSpaceSize x batch_size)
         valueIndices = CartesianIndex.(paddedPossibleValuesIdx, repeat(transpose(1:batchSize); outer=(maxActionSpaceSize, 1)))
-        #println("valueIndices: ", valueIndices)
     end
 
-    #println("valueIndices: ") 
-    #display(valueIndices)
     valueFeatures = nodeFeatures[:, valueIndices] #FxAxB
     
     f = size(valueFeatures, 1)
@@ -82,22 +63,15 @@ function (nn::VariableOutputCPNN)(state::BatchedDefaultTrajectoryState)
             end
         end
     end
-    #println("valueFeatures: ")
-    #display(valueFeatures)
-    #println("valueFeatures: ", size(valueFeatures)) 
-
+    
     # chain working on the node(s) feature(s)
     chainOutput = nn.nodeChain(hcat(variableFeatures, valueFeatures)) #F'x(A+1)xB where F' is the output size of nodeChain
-    #println("concatFeatures: ", size(hcat(variableFeatures, valueFeatures))) 
-    #println("chainOutput: ", size(chainOutput))
-   
+    
     variableOutput = nothing
     valueOutput = nothing
     Zygote.ignore() do
         variableOutput = reshape(chainOutput[:,1,:], (:,1,batchSize)) #F'xB
-        #println("variableOutput: ", size(variableOutput))
         valueOutput = chainOutput[:,2:end,:] #F'xAxB
-        #println("valueOutput: ", size(valueOutput))
     end
 
     finalInput = nothing 
@@ -105,26 +79,21 @@ function (nn::VariableOutputCPNN)(state::BatchedDefaultTrajectoryState)
         finalInput = []
         for i in 1:batchSize
             singleFinalInput = vcat(repeat(variableOutput[:,:,i], 1, maxActionSpaceSize), valueOutput[:,:,i])
-            #println("singleFinalInput: ", size(singleFinalInput))
             finalInput = isempty(finalInput) ? [singleFinalInput] : append!(finalInput, [singleFinalInput])
         end
     end
-    #finalInput: vector of matrices of size F'xA (total size BxF'xA)
     Zygote.ignore() do
         f, a = size(finalInput[1])
-        finalInput = reshape(collect(Iterators.flatten(finalInput)), (f, maxActionSpaceSize, batchSize)) #!!TO TEST #convert vector of matrices into a 3-dimensional matrix
-        #println("finalInput: ", size(finalInput)) 
+        finalInput = reshape(collect(Iterators.flatten(finalInput)), (f, maxActionSpaceSize, batchSize)) #!!TO TEST #convert vector of matrices into a 3-dimensional matrix 
     end
 
     output = dropdims(nn.outputChain(finalInput); dims=1) #AxB
-    #println("output: ")
-    #display(output)
-
     finalOutput = nothing
     Zygote.ignore() do
         finalOutput = reshape(
             Float32[-Inf32 for _ in 1:(size(state.allValuesIdx,1)*size(state.allValuesIdx,2))], 
-            size(state.allValuesIdx,1), size(state.allValuesIdx,2)
+            size(state.allValuesIdx,1), 
+            size(state.allValuesIdx,2)
         )
     end
     
@@ -140,8 +109,6 @@ function (nn::VariableOutputCPNN)(state::BatchedDefaultTrajectoryState)
         end
     end
     
-    #println("finalOutput: ", finalOutput)
-
     return finalOutput
 
 end
