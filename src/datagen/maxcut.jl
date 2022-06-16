8ec6e302cd5bd7f4d418eb83366246cf4e543e94
Tristan François <tpv.francois@gmail.com>
2022-06-16 16:41:16 -0400

    New maxcut modelisation

--- src/datagen/maxcut.jl
+++ src/datagen/maxcut.jl
@@ -19,14 +19,18 @@ function fill_with_generator!(cpmodel::CPModel, gen::MaxCutGenerator;  rng::Abst
 
     # create edge variable and constraints
     edge_vars = SeaPearl.IntVar[]
+    opposite_edge_vars = SeaPearl.IntVarViewOpposite[]
     for e in Graphs.edges(graph)
         push!(edge_vars, SeaPearl.IntVar(0, 1, "edge_" * string(e.src) * "->" * string(e.dst), cpmodel.trailer))
+        push!(opposite_edge_vars, SeaPearl.IntVarViewOpposite(last(edge_vars), "-edge_" * string(e.src) * "->" * string(e.dst)))
         SeaPearl.addVariable!(cpmodel, last(edge_vars); branchable=false)
         # We want e = (e.src != e.dst)
         # Since we can't write it like that in SeaPearl, we use the equivalence
-        # e = (e.src != e.dst) <=> (e - e.src - e.dst <= 0 && e + e.src + e.dst <= 2)
+        # e = (e.src != e.dst) <=> (-1 <= e - e.src - e.dst <= 0 && 1 <= e + e.src + e.dst <= 2
         SeaPearl.addConstraint!(cpmodel, SeaPearl.SumLessThan([last(edge_vars), opposite_node_vars[e.src], opposite_node_vars[e.dst]], 0, cpmodel.trailer))
+        SeaPearl.addConstraint!(cpmodel, SeaPearl.SumLessThan([last(opposite_edge_vars), node_vars[e.src], node_vars[e.dst]], 1, cpmodel.trailer))
         SeaPearl.addConstraint!(cpmodel, SeaPearl.SumLessThan([last(edge_vars), node_vars[e.src], node_vars[e.dst]], 2, cpmodel.trailer))
+        SeaPearl.addConstraint!(cpmodel, SeaPearl.SumLessThan([last(opposite_edge_vars), opposite_node_vars[e.src], opposite_node_vars[e.dst]], -1, cpmodel.trailer))
     end
 
     ### Objective ### minimize: -sum(edge_vars[i])
