5c78d18ae9674f923ec652b7b8371c33c8bb43bd
Tom Marty <59280588+3rdCore@users.noreply.github.com>
2022-07-08 16:36:07 -0400

    Max pooling for GraphConv  (#238)
    * added maxPooling for GraphConv
    
    * work on MaxPooling
    
    * fixed latin test
    
    * maxPooling for HFG
    
    * added doc for graphconv
    
    * added doc for HGC maxPooling
    
    * fix maxPool operation
    
    * fixed adjacency matrix type for FG and HFG
    
    * fix contovar valtovar def
    
    * fix graphconv with maxPooling on FG
    
    * fix MaxPooling
    
    * fix meanPooling
    
    * fixed hgc
    
    Co-authored-by: 3rdCore <tom.marty@polytec>

--- src/RL/utils/geometricflux/heterogeneousgraphconv.jl
+++ src/RL/utils/geometricflux/heterogeneousgraphconv.jl
@@ -62,26 +62,36 @@ function (g::HeterogeneousGraphConv{<:AbstractMatrix,<:Any,meanPooling})(fgs::Ba
     H1, H2, H3 = fgs.varnf, fgs.connf, fgs.valnf
     X1, X2, X3 = original_fgs.varnf, original_fgs.connf, original_fgs.valnf
 
-    sumcontovar, sumvaltovar, sumvartocon, sumvartoval = nothing, nothing, nothing, nothing
+    MatVar, MatCon, MatVal = nothing,nothing,nothing
     Zygote.ignore() do
         sumcontovar = replace(reshape(mapslices(x -> sum(eachrow(x)), contovar, dims=[1, 2]), 1, :, size(contovar, 3)), 0=>1)
         sumvaltovar = replace(reshape(mapslices(x -> sum(eachrow(x)), valtovar, dims=[1, 2]), 1, :, size(valtovar, 3)), 0=>1)
         sumvartocon = replace(reshape(mapslices(x -> sum(eachrow(x)), vartocon, dims=[1, 2]), 1, :, size(vartocon, 3)), 0=>1)
         sumvartoval = replace(reshape(mapslices(x -> sum(eachrow(x)), vartoval, dims=[1, 2]), 1, :, size(vartoval, 3)), 0=>1)
-    end
+    
     contovarN = contovar ./ sumcontovar
     valtovarN = valtovar ./ sumvaltovar
     vartoconN = vartocon ./ sumvartocon
     vartovalN = vartoval ./ sumvartoval
 
-    return BatchedHeterogeneousFeaturedGraph{Float32}(
+    MatVar =  vcat(X1, H1, H2 ⊠ contovarN, H3 ⊠ valtovarN)
+    MatCon = vcat(X2, H2, H1 ⊠ vartoconN)
+    MatVal =  vcat(X3, H3, H1 ⊠ vartovalN) 
+    end
+    XX1 = g.σ.(g.weightsvar ⊠ MatVar .+ g.biasvar)
+    XX2 = g.σ.(g.weightscon ⊠ MatCon .+ g.biascon)
+    XX3 = g.σ.(g.weightsval ⊠ MatVal .+ g.biasval)
+
+    Zygote.ignore() do
+        return BatchedHeterogeneousFeaturedGraph{Float32}(
         contovar,
         valtovar,
-        g.σ.(g.weightsvar ⊠ vcat(X1, H1, H2 ⊠ contovarN, H3 ⊠ valtovarN) .+ g.biasvar),
-        g.σ.(g.weightscon ⊠ vcat(X2, H2, H1 ⊠ vartoconN) .+ g.biascon),
-        g.σ.(g.weightsval ⊠ vcat(X3, H3, H1 ⊠ vartovalN) .+ g.biasval),
+        XX1,
+        XX2,
+        XX3,
         fgs.gf
     )
+    end
 end
 
 function (g::HeterogeneousGraphConv{<:AbstractMatrix,<:Any,meanPooling})(fg::HeterogeneousFeaturedGraph, original_fg::HeterogeneousFeaturedGraph)
@@ -94,25 +104,36 @@ function (g::HeterogeneousGraphConv{<:AbstractMatrix,<:Any,meanPooling})(fg::Het
     sumvaltovar = reshape(sum(eachrow(valtovar)), 1, :)
     sumvartocon = reshape(sum(eachrow(vartocon)), 1, :)
     sumvartoval = reshape(sum(eachrow(vartoval)), 1, :)
+    MatVar, MatCon, MatVal = nothing,nothing,nothing
     Zygote.ignore() do
         sumcontovar = replace(sumcontovar, 0=>1)
         sumvaltovar = replace(sumvaltovar, 0=>1)
         sumvartocon = replace(sumvartocon, 0=>1)
         sumvartoval = replace(sumvartoval, 0=>1)
-    end
-    contovarN = contovar ./ sumcontovar
-    valtovarN = valtovar ./ sumvaltovar
-    vartoconN = vartocon ./ sumvartocon
-    vartovalN = vartoval ./ sumvartoval
 
+        contovarN = contovar ./ sumcontovar
+        valtovarN = valtovar ./ sumvaltovar
+        vartoconN = vartocon ./ sumvartocon
+        vartovalN = vartoval ./ sumvartoval
+
+        MatVar = vcat(X1, H1, H2 * contovarN, H3 * valtovarN)
+        MatCon = vcat(X2, H2, H1 * vartoconN)
+        MatVal =  vcat(X3, H3, H1 * vartovalN) 
+    end
+
+    XX1 = g.σ.(g.weightsvar * MatVar .+ g.biasvar)
+    XX2 = g.σ.(g.weightscon * MatCon .+ g.biascon)
+    XX3 = g.σ.(g.weightsval * MatVal.+ g.biasval)
+    Zygote.ignore() do
     return HeterogeneousFeaturedGraph(
         contovar,
         valtovar,
-        g.σ.(g.weightsvar * vcat(X1, H1, H2 * contovarN, H3 * valtovarN) .+ g.biasvar),
-        g.σ.(g.weightscon * vcat(X2, H2, H1 * vartoconN) .+ g.biascon),
-        g.σ.(g.weightsval * vcat(X3, H3, H1 * vartovalN) .+ g.biasval),
+        XX1,
+        XX2,
+        XX3,
         fg.gf
     )
+    end
 end
 
 """
@@ -128,6 +149,10 @@ function (g::HeterogeneousGraphConv{<:AbstractMatrix,<:Any, maxPooling})(fgs::Ba
     H1, H2, H3 = fgs.varnf, fgs.connf, fgs.valnf
     X1, X2, X3 = original_fgs.varnf, original_fgs.connf, original_fgs.valnf
     contovarN, valtovarN, vartoconN, vartovalN = contovar, valtovar, vartocon, vartoval
+    filteredembcontovar = nothing
+    filteredembvartocon = nothing
+    filteredembvaltovar = nothing
+    filteredembvartoval = nothing
 
     Zygote.ignore() do
         contovarIdx = repeat(collect(1:size(contovar,1)),1,size(contovar,2)).*contovar
@@ -135,27 +160,25 @@ function (g::HeterogeneousGraphConv{<:AbstractMatrix,<:Any, maxPooling})(fgs::Ba
         vartoconIdx = repeat(collect(1:size(vartocon,1)),1,size(vartocon,2)).*vartocon
         vartovalIdx = repeat(collect(1:size(vartoval,1)),1,size(vartoval,2)).*vartoval
 
-        contovarIdx = collect.(zip(contovarIdx,cat(repeat([1],size(contovar)[1:2]...),repeat([2], size(contovar)[1:2]...), dims= 3)))
-        valtovarIdx = collect.(zip(valtovarIdx,cat(repeat([1],size(vartoval)[1:2]...),repeat([2], size(valtovar)[1:2]...), dims= 3)))
-        vartoconIdx = collect.(zip(vartoconIdx,cat(repeat([1],size(vartocon)[1:2]...),repeat([2], size(vartocon)[1:2]...), dims= 3)))
-        vartovalIdx = collect.(zip(vartovalIdx,cat(repeat([1],size(vartoval)[1:2]...),repeat([2], size(vartoval)[1:2]...), dims= 3)))
+        contovarIdx = collect.(zip(contovarIdx,cat([repeat([i],size(contovar)[1:2]...) for i in 1:size(contovar)[3]]..., dims= 3)))
+        valtovarIdx = collect.(zip(valtovarIdx,cat([repeat([i],size(valtovar)[1:2]...) for i in 1:size(valtovar)[3]]..., dims= 3)))
+        vartoconIdx = collect.(zip(vartoconIdx,cat([repeat([i],size(vartocon)[1:2]...) for i in 1:size(vartocon)[3]]..., dims= 3)))
+        vartovalIdx = collect.(zip(vartovalIdx,cat([repeat([i],size(vartoval)[1:2]...) for i in 1:size(vartoval)[3]]..., dims= 3)))
 
         filteredcolcontovar = mapslices( x -> map(z-> filter(y -> y[1]!=0, z), eachcol(x)), contovarIdx, dims=[1,2])
         filteredcolvaltovar = mapslices( x -> map(z-> filter(y -> y[1]!=0, z), eachcol(x)), valtovarIdx, dims=[1,2])
         filteredcolvartocon = mapslices( x -> map(z-> filter(y -> y[1]!=0, z), eachcol(x)), vartoconIdx, dims=[1,2])
         filteredcolvartoval = mapslices( x -> map(z-> filter(y -> y[1]!=0, z), eachcol(x)), vartovalIdx, dims=[1,2])
 
-        filteredembcontovar = mapslices(x->map(y-> maximum(mapreduce(z->H2[:,z...], hcat, y), dims =2),  x), filteredcolcontovar, dims = [1,3])
-        filteredembvaltovar = mapslices(x->map(y-> maximum(mapreduce(z->H3[:,z...], hcat, y), dims =2),  x), filteredcolvaltovar, dims = [1,3])
-        filteredembvartocon = mapslices(x->map(y-> maximum(mapreduce(z->H1[:,z...], hcat, y), dims =2),  x), filteredcolvartocon, dims = [1,3])
-        filteredembvartoval  = mapslices(x->map(y-> maximum(mapreduce(z->H1[:,z...], hcat, y), dims =2),  x), filteredcolvartoval , dims = [1,3])
-
-        filteredembcontovar = reshape(reduce(hcat ,filteredembcontovar), size(H2))
-        filteredembvaltovar = reshape(reduce(hcat ,filteredembvaltovar), size(H3))
-        filteredembvartocon = reshape(reduce(hcat ,filteredembvartocon), size(H1))
-        filteredembvartoval = reshape(reduce(hcat ,filteredembvartoval), size(H1))
+        filteredembcontovar = mapslices(x->map(y-> !isempty(y) ? reshape(Base.maximum(mapreduce(z->H2[:,z...], hcat, y), dims =2),:) : zeros(Float32,size(H2,1)),  x), filteredcolcontovar, dims = [1,3])
+        filteredembvaltovar = mapslices(x->map(y-> !isempty(y) ? reshape(Base.maximum(mapreduce(z->H3[:,z...], hcat, y), dims =2), :) : zeros(Float32,size(H3,1)),  x), filteredcolvaltovar, dims = [1,3])
+        filteredembvartocon = mapslices(x->map(y-> !isempty(y) ? reshape(Base.maximum(mapreduce(z->H1[:,z...], hcat, y), dims =2), :) : zeros(Float32,size(H1,1)),  x), filteredcolvartocon, dims = [1,3])
+        filteredembvartoval  = mapslices(x->map(y-> !isempty(y) ? reshape(Base.maximum(mapreduce(z->H1[:,z...], hcat, y), dims =2),:) : zeros(Float32,size(H1,1)),  x), filteredcolvartoval , dims = [1,3])
+        filteredembcontovar = reshape(reduce(hcat ,filteredembcontovar), :,size(H1)[2:3]...)
+        filteredembvaltovar = reshape(reduce(hcat ,filteredembvaltovar), :,size(H1)[2:3]...)
+        filteredembvartocon = reshape(reduce(hcat ,filteredembvartocon), :,size(H2)[2:3]...)
+        filteredembvartoval = reshape(reduce(hcat ,filteredembvartoval), :,size(H3)[2:3]...)
     end
-
     return BatchedHeterogeneousFeaturedGraph{Float32}(
         contovar,
         valtovar,
@@ -165,7 +188,6 @@ function (g::HeterogeneousGraphConv{<:AbstractMatrix,<:Any, maxPooling})(fgs::Ba
         fgs.gf
     )
 end
-
 """
 function (g::HeterogeneousGraphConv{<:AbstractMatrix,<:Any, maxPooling})(fg::HeterogeneousFeaturedGraph, original_fg::HeterogeneousFeaturedGraph)
 
@@ -179,6 +201,10 @@ function (g::HeterogeneousGraphConv{<:AbstractMatrix,<:Any, maxPooling})(fg::Het
     
     H1, H2, H3 = fg.varnf, fg.connf, fg.valnf
     X1, X2, X3 = original_fg.varnf, original_fg.connf, original_fg.valnf
+    filteredembcontovar = nothing
+    filteredembvaltovar = nothing
+    filteredembvartocon = nothing
+    filteredembvartoval = nothing
     Zygote.ignore() do      
         contovarIdx = repeat(collect(1:size(contovar,1)),1,size(contovar,2)).*contovar
         valtovarIdx = repeat(collect(1:size(valtovar,1)),1,size(valtovar,2)).*valtovar
@@ -190,17 +216,18 @@ function (g::HeterogeneousGraphConv{<:AbstractMatrix,<:Any, maxPooling})(fg::Het
         filteredcolvartocon = map(x-> filter(y -> y!=0,x),eachcol(vartoconIdx))
         filteredcolvartoval = map(x-> filter(y -> y!=0,x),eachcol(vartovalIdx))
 
-        filteredembcontovar = mapreduce(x->maximum(H2[:,x],dims = 2), hcat,filteredcolcontovar)
-        filteredembvaltovar = mapreduce(x->maximum(H3[:,x],dims = 2), hcat,filteredcolvaltovar)
-        filteredembvartocon = mapreduce(x->maximum(H1[:,x],dims = 2), hcat,filteredcolvartocon)
-        filteredembvartoval = mapreduce(x->maximum(H1[:,x],dims = 2), hcat,filteredcolvartoval)
+        filteredembcontovar = mapreduce(x->!isempty(x) ? Base.maximum(H2[:,x], dims = 2) : zeros(Float32,size(H2,1)), hcat, filteredcolcontovar)
+        filteredembvaltovar = mapreduce(x->!isempty(x) ? Base.maximum(H3[:,x], dims = 2) : zeros(Float32,size(H3,1)), hcat, filteredcolvaltovar)
+        filteredembvartocon = mapreduce(x->!isempty(x) ? Base.maximum(H1[:,x], dims = 2) : zeros(Float32,size(H1,1)), hcat, filteredcolvartocon)
+        filteredembvartoval = mapreduce(x->!isempty(x) ? Base.maximum(H1[:,x], dims = 2) : zeros(Float32,size(H1,1)), hcat, filteredcolvartoval)
     end
     return HeterogeneousFeaturedGraph(
         contovar,
         valtovar,
-        g.σ.(g.weightsvar * vcat(X1, H1, filteredembcontovar, filteredcolvaltovar) .+ g.biasvar),
+        g.σ.(g.weightsvar * vcat(X1, H1, filteredembcontovar, filteredembvaltovar) .+ g.biasvar),
         g.σ.(g.weightscon * vcat(X2, H2, filteredembvartocon) .+ g.biascon),
         g.σ.(g.weightsval * vcat(X3, H3, filteredembvartoval) .+ g.biasval),
         fg.gf
     )
+    
 end
