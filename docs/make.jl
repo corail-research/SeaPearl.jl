c7ff16b18203f8dcc4f78da8be102d3782c8d588
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-01-11 15:29:36 -0500

    Add internals documentation

--- docs/make.jl
+++ docs/make.jl
@@ -11,10 +11,11 @@ makedocs(
         "Constraints" => "CP/constraints.md",
         "Variables" => "CP/variables.md",
         "Trailer" => "CP/trailer.md",
+        "Internals" => CP/internals.md,
     ],
     "Reinforcement Learning" => "reinforcement_learning.md",
-    "Building Models" =>
-      ["Basics" => "models/basics.md"],
+    # "Building Models" =>
+    #   ["Basics" => "models/basics.md"],
     "Community" => "community.md"],
 )
 
---
+++ docs/src/CP/internals.md
@@ -0,0 +1,21 @@
+# SeaPearl Internals
+
+## CPModel
+
+```@docs
+SeaPearl.Statistics
+SeaPearl.Limit
+SeaPearl.CPModel
+SeaPearl.addVariable!
+SeaPearl.addObjective!
+SeaPearl.addKnownObjective!
+SeaPearl.addConstraint!
+SeaPearl.is_branchable
+SeaPearl.branchable_variables
+SeaPearl.solutionFound
+SeaPearl.triggerFoundSolution!
+SeaPearl.triggerInfeasible!
+SeaPearl.tightenObjective!
+Base.isempty
+Base.empty!
+```
