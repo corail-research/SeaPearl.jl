1a678fe3fe121b184006deed0a61992195abf65d
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-01-17 16:55:43 -0500

    Add RL tests

--- test/CP/CP.jl
+++ test/CP/CP.jl
@@ -1,20 +1,11 @@
 @testset "CP.jl" begin
-
     include("variables/variables.jl")
     include("core/model.jl")
     include("constraints/constraints.jl")
-
     include("core/fixPoint.jl")
     include("core/cpModification.jl")
-
     include("variableselection/variableselection.jl")
-
     include("valueselection/valueselection.jl")
-
     include("core/search/search.jl")
     include("core/solver.jl")
-
-    # @testset "solve()" begin
-     #    @test (@test_logs (:info, "Solved !") SeaPearl.solve()) == nothing
-    # end
 end
