93b18d3ab1b74e050fcf222a8d004ea80b361053
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-01-17 17:21:25 -0500

    Add RL tests (#316)

--- test/RL/RL.jl
+++ test/RL/RL.jl
@@ -1,11 +1,10 @@
 using Random
 
-@testset "RL.jl" begin
 
+@testset "RL.jl" begin
     include("representation/representation.jl")
     include("utils/heterogeneousfeaturedgraph.jl")
     include("utils/batchedheterogeneousfeaturedgraph.jl")
     include("nn_structures/nn_structures.jl")
     include("utils.jl")
-
 end
