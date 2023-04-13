cd6d6d6aea02128616c4994c3fac276f1642fe32
navaxel <104438099+navaxel@users.noreply.github.com>
2023-04-13 14:25:04 -0400

    1st constraint (#327)
    * greater and less constraints
    
    * all equal constraint + test
    
    * Update equal.jl
    
    Modification on comments and deletion of redundant tests.

--- src/CP/constraints/equal.jl
+++ src/CP/constraints/equal.jl
@@ -135,3 +135,95 @@ end
 function Base.show(io::IO, con::Equal)
     print(io, typeof(con), ": ", con.x.id, " == ", con.y.id)
 end
+
+
+"""
+    AllEqual(x::Array{<:AbstractIntVar}, trailer::SeaPearl.Trailer)
+
+Equality constraint between array variables, stating that `x[1] == x[2] == ... == x[n]`.
+"""
+struct AllEqual <: EqualConstraint
+    x       ::Array{<:AbstractIntVar}
+    active  ::StateObject{Bool}
+    function AllEqual(x::Array{<:AbstractIntVar}, trailer::Trailer)
+        constraint = new(x, StateObject(true, trailer))
+        for i in 1:length(x)
+            addOnDomainChange!(x[i], constraint)
+        end
+        return constraint
+    end
+end
+
+"""
+    propagate!(constraint::AllEqual, toPropagate::Set{Constraint}, prunedDomains::CPModification)
+
+`AllEqual` propagation function.
+"""
+function propagate!(constraint::AllEqual, toPropagate::Set{Constraint}, prunedDomains::CPModification)
+    if !constraint.active.value
+        return true
+    end
+
+    prunedX = pruneAllEqual!(constraint.x)
+
+    for i in 1:length(constraint.x)
+        if !isempty(prunedX[i])
+            triggerDomainChange!(toPropagate, constraint.x[i])
+            addToPrunedDomains!(prunedDomains, constraint.x[i], prunedX[i])
+        end
+    end
+
+    if constraint in toPropagate
+        pop!(toPropagate, constraint)
+    end
+
+    if length(constraint.x[1].domain) <= 1
+        setValue!(constraint.active, false)
+    end
+    
+    if isempty(constraint.x[1].domain)
+        return false
+    end
+    return true
+end
+
+function pruneAllEqual!(x::Array{<:AbstractIntVar})
+    valuecounts = Dict{Int64,Int64}()
+    n = length(x)
+    for i in 1:n
+        for val in x[i].domain
+            if haskey(valuecounts, val)
+                valuecounts[val] += 1
+            else
+                valuecounts[val] = 1
+            end
+        end
+    end
+    toRemove = Dict{Int, Array{Int}}()
+    for i in 1:n
+        toRemove[i] = Int[]
+        for val in x[i].domain
+            if valuecounts[val] != n
+                push!(toRemove[i], val)
+            end
+        end
+        for val in toRemove[i]
+            remove!(x[i].domain, val)
+        end
+    end
+    return toRemove
+end
+
+
+variablesArray(constraint::AllEqual) = constraint.x
+
+function Base.show(io::IO, ::MIME"text/plain", con::AllEqual)
+    print(io, string(typeof(con)), ": ", join([var.id for var in con.x], " == "), ", active = ", con.active)
+    for var in con.x
+        print(io, "\n   ", var)
+    end
+end
+
+function Base.show(io::IO, con::AllEqual)
+    print(io, string(typeof(con)), ": ", join([var.id for var in con.x], " == "))
+end
--- src/CP/constraints/greaterorequal.jl
+++ src/CP/constraints/greaterorequal.jl
@@ -35,3 +35,129 @@ end
 function Base.show(io::IO, con::GreaterOrEqualConstant)
     print(io, typeof(con), ": ", con.x.id, " ≥ ", con.v)
 end
+
+
+
+
+
+"""
+    GreaterOrEqual(x::AbstractIntVar, y::AbstractIntVar, trailer::Trailer)
+
+Inequality between variables constraint, states that `x >= y`
+"""
+struct GreaterOrEqual <: Constraint
+    x       ::AbstractIntVar
+    y       ::AbstractIntVar
+    active  ::StateObject{Bool}
+
+    """
+        GreaterOrEqual(x::AbstractIntVar, y::AbstractIntVar, trailer::Trailer)
+
+    Inequality between variables constraint, states that `x >= y`
+    """
+    function GreaterOrEqual(x::AbstractIntVar, y::AbstractIntVar, trailer)
+        constraint = new(x, y, StateObject(true, trailer))
+        addOnDomainChange!(x, constraint)
+        addOnDomainChange!(y, constraint)
+        return constraint
+    end
+end
+
+"""
+    propagate!(constraint::GreaterOrEqual, toPropagate::Set{Constraint}, prunedDomains::CPModification)
+
+`GreaterOrEqual` propagation function.
+"""
+function propagate!(constraint::GreaterOrEqual, toPropagate::Set{Constraint}, prunedDomains::CPModification)
+
+
+    if minimum(constraint.x.domain) >= maximum(constraint.y.domain)
+        setValue!(constraint.active, false)
+    end
+
+    prunedX = removeBelow!(constraint.x.domain, minimum(constraint.y.domain))
+    if !isempty(prunedX)
+        addToPrunedDomains!(prunedDomains, constraint.x, prunedX)
+        triggerDomainChange!(toPropagate, constraint.x)
+    end
+
+    prunedY = removeAbove!(constraint.y.domain, maximum(constraint.x.domain))
+    if !isempty(prunedY)
+        addToPrunedDomains!(prunedDomains, constraint.y, prunedY)
+        triggerDomainChange!(toPropagate, constraint.y)
+    end
+    return !isempty(constraint.x.domain) && !isempty(constraint.y.domain)
+end
+
+variablesArray(constraint::GreaterOrEqual) = [constraint.x, constraint.y]
+
+function Base.show(io::IO, ::MIME"text/plain", con::GreaterOrEqual)
+    println(io, typeof(con), ": ", con.x.id, " ≥ ", con.y.id, ", active = ", con.active)
+    println(io, "   ", con.x)
+    println(io, "   ", con.y)
+end
+
+function Base.show(io::IO, con::GreaterOrEqual)
+    print(io, typeof(con), ": ", con.x.id, " ≥ ", con.y.id)
+end
+
+"""
+    Greater(x::AbstractIntVar, y::AbstractIntVar, trailer::Trailer)
+
+Inequality between variables constraint, states that `x > y`
+"""
+struct Greater <: Constraint
+    x       ::AbstractIntVar
+    y       ::AbstractIntVar
+    active  ::StateObject{Bool}
+
+    """
+        Greater(x::AbstractIntVar, y::AbstractIntVar, trailer::Trailer)
+
+    Inequality between variables constraint, states that `x > y`
+    """
+    function Greater(x::AbstractIntVar, y::AbstractIntVar, trailer)
+        constraint = new(x, y, StateObject(true, trailer))
+        addOnDomainChange!(x, constraint)
+        addOnDomainChange!(y, constraint)
+        return constraint
+    end
+end
+
+"""
+    propagate!(constraint::Greater, toPropagate::Set{Constraint}, prunedDomains::CPModification)
+
+`Greater` propagation function.
+"""
+function propagate!(constraint::Greater, toPropagate::Set{Constraint}, prunedDomains::CPModification)
+
+
+    if minimum(constraint.x.domain) > maximum(constraint.y.domain)
+        setValue!(constraint.active, false)
+    end
+
+    prunedX = removeBelow!(constraint.x.domain, minimum(constraint.y.domain)+1)
+    if !isempty(prunedX)
+        addToPrunedDomains!(prunedDomains, constraint.x, prunedX)
+        triggerDomainChange!(toPropagate, constraint.x)
+    end
+
+    prunedY = removeAbove!(constraint.y.domain, maximum(constraint.x.domain)-1)
+    if !isempty(prunedY)
+        addToPrunedDomains!(prunedDomains, constraint.y, prunedY)
+        triggerDomainChange!(toPropagate, constraint.y)
+    end
+    return !isempty(constraint.x.domain) && !isempty(constraint.y.domain)
+end
+
+variablesArray(constraint::Greater) = [constraint.x, constraint.y]
+
+function Base.show(io::IO, ::MIME"text/plain", con::Greater)
+    println(io, typeof(con), ": ", con.x.id, " > ", con.y.id, ", active = ", con.active)
+    println(io, "   ", con.x)
+    println(io, "   ", con.y)
+end
+
+function Base.show(io::IO, con::Greater)
+    print(io, typeof(con), ": ", con.x.id, " > ", con.y.id)
+end
