892bf34576c525995a64ea816f57585614c71f54
navaxel <axel.navarro@polytechnique.edu>
2023-04-13 14:50:26 -0400

    addition constraint y = sum(x)

--- test/CP/constraints/sumtozero.jl
+++ test/CP/constraints/sumtozero.jl
@@ -50,4 +50,55 @@
 
 
     end
+    @testset "Addition()" begin
+        trailer = SeaPearl.Trailer()
+
+        x1 = SeaPearl.IntVar(2, 6, "x1", trailer)
+        x2 = SeaPearl.IntVar(2, 6, "x2", trailer)
+        x3 = SeaPearl.IntVar(2, 6, "x3", trailer)
+        x = Vector{SeaPearl.AbstractIntVar}([x1, x2, x3])
+
+        y = SeaPearl.IntVar(2, 3, "y", trailer)
+
+        
+        constraint = SeaPearl.Addition(y, x, trailer)
+
+        @test constraint in x2.onDomainChange
+        @test constraint in y.onDomainChange
+        @test constraint.active.value
+        @test constraint.numberOfFreeVars.value == 4
+        @test constraint.sumOfFixedVars.value == 0
+        @test constraint.freeIds == [1, 2, 3, 4]
+    end
+    @testset "propagate!(::Addition)" begin
+        trailer = SeaPearl.Trailer()
+
+        x1 = SeaPearl.IntVar(0, 3, "x1", trailer)
+        x2 = SeaPearl.IntVar(1, 5, "x2", trailer)
+        x3 = SeaPearl.IntVar(0, 2, "x3", trailer)
+        x = Vector{SeaPearl.AbstractIntVar}([x1, x2, x3])
+
+        y = SeaPearl.IntVar(0, 1, "y", trailer)
+        
+        constraint = SeaPearl.Addition(y, x, trailer)
+
+        toPropagate = Set{SeaPearl.Constraint}()
+        prunedDomains = SeaPearl.CPModification()
+
+        @test SeaPearl.propagate!(constraint, toPropagate, prunedDomains)
+
+        @test length(y.domain) == 1
+        @test 1 in y.domain
+        @test !(3 in x1.domain)
+        @test !(1 in x3.domain)
+        @test prunedDomains == SeaPearl.CPModification(
+            "x1"  => [1,2,3],
+            "x2"  => [2,3,4,5],
+            "x3"  => [1,2],
+            "-y" => [0],
+            "y" => [0]
+        )
+
+    end
 end
+
