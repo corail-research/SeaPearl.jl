9c9a0ddee30bc8cb21c14c4444636ad8e4c6acd9
navaxel <axel.navarro@polytechnique.edu>
2023-05-04 14:50:50 -0400

    add constant constraint to intension parsing

--- test/CP/constraints/lessorequal.jl
+++ test/CP/constraints/lessorequal.jl
@@ -102,4 +102,39 @@
         @test !SeaPearl.propagate!(constraint2, toPropagate, prunedDomains)
 
     end
+
+    @testset "LessConstant()" begin
+        trailer = SeaPearl.Trailer()
+        x = SeaPearl.IntVar(2, 6, "x", trailer)
+
+        constraint = SeaPearl.LessConstant(x, 4, trailer)
+
+        @test constraint in x.onDomainChange
+        @test constraint.active.value
+    end
+    @testset "propagate!(::LessOrEqualConstant)" begin
+        trailer = SeaPearl.Trailer()
+        x = SeaPearl.IntVar(2, 6, "x", trailer)
+
+        constraint = SeaPearl.LessConstant(x, 4, trailer)
+
+        toPropagate = Set{SeaPearl.Constraint}()
+        prunedDomains = SeaPearl.CPModification()
+
+        @test SeaPearl.propagate!(constraint, toPropagate, prunedDomains)
+
+        @test length(x.domain) == 2
+        @test 2 in x.domain
+        @test !(4 in x.domain)
+        @test prunedDomains == SeaPearl.CPModification("x" => [4, 5, 6])
+
+        y = SeaPearl.IntVar(2, 2, "y", trailer)
+
+        cons2 = SeaPearl.LessOrEqualConstant(y, 1, trailer)
+
+        @test !SeaPearl.propagate!(cons2, toPropagate, prunedDomains)
+
+        @test isempty(y.domain)
+
+    end
 end
