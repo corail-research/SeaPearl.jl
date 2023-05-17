f4bba98005d1934eceea77350e3a40a97ef8fcfd
navaxel <104438099+navaxel@users.noreply.github.com>
2023-05-17 14:52:49 -0400

    test all parsing constraint, variable and objective (#361)

--- test/parser/constraints/constraints.jl
+++ test/parser/constraints/constraints.jl
@@ -1,2 +1,4 @@
 include("allDifferent_parser.jl")
 include("extension_parser.jl")
+include("intension_parser.jl")
+include("sum_parser.jl")
---
+++ test/parser/objective/objective_parser.jl
@@ -0,0 +1,41 @@
+@testset "objective_parser.jl" begin
+    @testset "objective_simple" begin
+        filename = "./parser/objective/data/objective_simple.xml"
+
+        model, trailer, dict_var = SeaPearl.parse_xml_file(filename)                    
+
+        @test isa(model.objective, SeaPearl.IntVar)
+        @test model.objective == model.variables["z"]
+
+    end
+
+    @testset "objective_sum" begin
+        filename = "./parser/objective/data/objective_sum.xml"
+
+        model, trailer, dict_var = SeaPearl.parse_xml_file(filename)                    
+
+        @test model.objective.id == "objective_1"
+        @test isa(model.objective, SeaPearl.IntVar)
+        @test maximum(model.objective.domain) == 7
+
+        @test length(model.constraints) == 1
+        @test isa(model.constraints[1], SeaPearl.SumToZero)
+        for i = 1:length(dict_var["x"])
+            @test isa(model.constraints[1].x[i],  SeaPearl.IntVarViewMul)
+        end
+    end
+
+    @testset "objective_nValues" begin
+        filename = "./parser/objective/data/objective_nValues.xml"
+
+        model, trailer, dict_var = SeaPearl.parse_xml_file(filename)
+
+        @test model.objective.id == "x_nValues"
+        @test isa(model.objective, SeaPearl.IntVar)
+        @test maximum(model.objective.domain) == 36
+
+        @test length(model.constraints) == 1
+        @test isa(model.constraints[1], SeaPearl.NValuesConstraint)
+        @test length(model.constraints[1].x) == 200
+    end
+end
