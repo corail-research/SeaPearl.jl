df25bdf8a1b5a59b4da0b74be2be06ab53d4e43e
navaxel <104438099+navaxel@users.noreply.github.com>
2023-07-04 15:26:22 -0400

    Correction for intension constraint parsing with constant (#418)

--- test/parser/constraints/intension_parser.jl
+++ test/parser/constraints/intension_parser.jl
@@ -25,4 +25,15 @@
         # Test whether the domains of the variables in the intension constraint match the domains specified in the XML file
         @test SeaPearl.maximum(model.constraints[4].x.domain) == 35
     end
+
+    @testset "intension_constant" begin
+        filename = "./parser/constraints/data/intension/intension_constant.xml"
+
+        model, trailer, dict_var = SeaPearl.parse_xml_file(filename)                    
+
+        # Test whether the constraints have been correctly added to the model
+        @test length(model.constraints) == 7
+        @test isa(model.constraints[2], SeaPearl.GreaterOrEqual)
+        @test isa(model.constraints[1].x, SeaPearl.IntVarViewMul)
+    end
 end
